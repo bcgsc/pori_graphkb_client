b8f7f1c1962375f0414f943272fe6fbacc856231
Isaac Beckie <isaacbeckie@gmail.com>
2018-05-24 16:47:18 -0700

    table horizontal scrolling workaround

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,3 +4,5 @@ export { NodeViewComponent } from './node-view/node-view.component';
 export { QueryViewComponent } from './query-view/query-view.component';
 export { TableViewComponent } from './table-view/table-view.component';
 export { TreeViewComponent } from './tree-view/tree-view.component';
+export { PaginationComponent } from './pagination.component/pagination.component';
+export { MyTableComponent } from './my-table.component/table.component';

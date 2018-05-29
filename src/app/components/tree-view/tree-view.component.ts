026913a8fdbfad2c727c3984def10536cb611b5f
Isaac Beckie <ibeckie@bcgsc.ca>
2018-05-29 13:16:52 -0700

    added functionality for the tree view to locate the selected node if it is selected in another view, and expand to that point

--- src/app/components/tree-view/tree-view.component.ts
+++ src/app/components/tree-view/tree-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, SimpleChanges } from '@angular/core';
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { MatTreeFlattener, MatTreeFlatDataSource } from '@angular/material';
 import { BehaviorSubject, Observable, of as observableOf } from 'rxjs';
@@ -30,17 +30,33 @@ export class TreeViewComponent implements OnInit {
 
   ngOnInit() {
     this.dataSource.data = this.data;
-    this.treeControl.expandDescendants(this.data[0]);
-    this.treeControl.collapseDescendants(this.selectedNode);
-    // let level = this._getLevel(this.selectedNode);
-    // let children = this.treeControl.getDescendants(this.data[0]);
-    // let parents = this.selectedNode.parents;
+    this.data.forEach(root => {
+      this.expandToSelected(root)
+    });
+  }
 
-    // children.forEach(child => {
-    //   if (child['level'] == level - 1 && !parents.includes(child['@rid'])) {
-    //     this.treeControl.collapseDescendants(child);
-    //   }
-    // })
+  ngOnChanges(changes: SimpleChanges) {
+    if (!changes.selectedNode) return;
+    this.treeControl.collapseAll();
+    this.data.forEach(root => {
+      this.expandToSelected(root);
+    });
+  }
+
+  private expandToSelected(rn): boolean {
+    if (rn['@rid'] == this.selectedNode["@rid"]) {
+      return true;
+    }
+    if (!rn._children) return false;
+
+    let ret = false;
+    rn._children.forEach(child => {
+      if (this.expandToSelected(child)) {
+        ret = true;
+        this.treeControl.expand(rn);
+      }
+    });
+    return ret;
   }
 
   transformer = (node: DiseaseTerm, level: number) => {

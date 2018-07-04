aeae1890720e30bad79fcdb34d67265b5474590d
Isaac Beckie <ibeckie@bcgsc.ca>
2018-07-04 12:35:47 -0700

    Lint SVGLink

--- src/components/SVGLink/SVGLink.js
+++ src/components/SVGLink/SVGLink.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './SVGLink.css';
 
 class SVGLink extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            endMarker: 'url(#arrow)',
-            startMarker:(props.link.type==='alias') ? 'url(#darrow': '',
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <g>
-                <path
-                    className="link"
-                    d={"M" + (this.props.link.source.x || 0) + " " + (this.props.link.source.y || 0) 
-                        + "L" + (this.props.link.target.x || 0) + " " + (this.props.link.target.y || 0)
-                    }
-                    markerEnd={this.state.endMarker}
-                    markerStart={this.state.startMarker}
-                />
-            </g>
-        )
-    }
+    this.state = {
+      endMarker: 'url(#arrow)',
+      startMarker: (props.link.type === 'alias') ? 'url(#darrow' : '',
+    };
+  }
 
-} export default SVGLink
+  render() {
+    const { link } = this.props;
+    const { endMarker, startMarker } = this.state;
+    return (
+      <g>
+        <path
+          className="link"
+          d={`M${(link.source.x || 0)} ${(link.source.y || 0)}L${(link.target.x || 0)} ${(link.target.y || 0)}`}
+          markerEnd={endMarker}
+          markerStart={startMarker}
+        />
+      </g>
+    );
+  }
+}
+
+SVGLink.propTypes = {
+  link: PropTypes.object.isRequired,
+};
+
+export default SVGLink;

56104044de86608887577aa2051613c89f11d570
Isaac Beckie <ibeckie@bcgsc.ca>
2018-07-03 18:51:45 -0700

    ESlint api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,38 @@
-"use strict";
+import * as jc from 'json-cycle';
+import auth from './auth';
 
-import axios from "axios";
-import promise from "promise";
-import auth from "./auth";
-import * as jc from "json-cycle";
-
-const API_BASE_URL = "http://kbapi01:8008/api/v0.0.8";
+const API_BASE_URL = 'http://kbapi01:8008/api/v0.0.8';
+const CACHE_EXPIRY = 8;
 
 export default class api {
   static getHeaders() {
     const headers = new Headers();
-    headers.append("Content-type", "application/json");
+    headers.append('Content-type', 'application/json');
     if (auth.getToken()) {
-      headers.append("Authorization", auth.getToken());
+      headers.append('Authorization', auth.getToken());
     }
     return headers;
   }
 
   static patch(endpoint, payload) {
     const init = {
-      method: "PATCH",
-      body: JSON.stringify(payload)
+      method: 'PATCH',
+      body: JSON.stringify(payload),
     };
     return api.fetchWithInterceptors(endpoint, init);
   }
 
   static get(endpoint) {
     const init = {
-      method: "GET"
+      method: 'GET',
     };
     return api.fetchWithInterceptors(endpoint, init);
   }
 
   static post(endpoint, payload) {
     const init = {
-      method: "POST",
-      body: JSON.stringify(payload)
+      method: 'POST',
+      body: JSON.stringify(payload),
     };
 
     return api.fetchWithInterceptors(endpoint, init);
@@ -43,7 +40,7 @@ export default class api {
 
   static delete(endpoint) {
     const init = {
-      method: "DELETE"
+      method: 'DELETE',
     };
 
     return api.fetchWithInterceptors(endpoint, init);
@@ -52,46 +49,43 @@ export default class api {
   static fetchWithInterceptors(endpoint, init) {
     const initWithInterceptors = {
       ...init,
-      headers: api.getHeaders()
+      headers: api.getHeaders(),
     };
     return fetch(new Request(API_BASE_URL + endpoint, initWithInterceptors))
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
-        } else {
-          return promise.reject(response);
         }
+        return Promise.reject(response);
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.status === 401) {
           auth.clearToken();
         }
-        return promise.reject(error);
+        return Promise.reject(error);
       });
   }
 
   static getEdgeTypes() {
-    const edgeTypes = localStorage.getItem("edgeTypes");
-    const edgeTypeExpiry = localStorage.getItem("edgeTypesExpiry");
+    const edgeTypes = localStorage.getItem('edgeTypes');
+    const edgeTypeExpiry = localStorage.getItem('edgeTypesExpiry');
     if (
-      !edgeTypes ||
-      (edgeTypes && edgeTypeExpiry && edgeTypeExpiry < Date.now().valueOf()) ||
-      !edgeTypeExpiry
+      !edgeTypes
+      || (edgeTypes && edgeTypeExpiry && edgeTypeExpiry < Date.now().valueOf())
+      || !edgeTypeExpiry
     ) {
       return api.loadEdges();
-    } else {
-      return Promise.resolve(JSON.parse(edgeTypes));
     }
+    return Promise.resolve(JSON.parse(edgeTypes));
   }
 
   static loadEdges() {
-    return api.get("/schema").then(response => {
-      response = jc.retrocycle(response.schema);
+    return api.get('/schema').then((response) => {
+      const cycled = jc.retrocycle(response.schema);
       const list = [];
-      Object.keys(response).forEach(key => {
+      Object.keys(cycled).forEach((key) => {
         if (
-          response[key].inherits.includes("E") &&
-          response[key].inherits.includes("OntologyEdge")
+          cycled[key].inherits.includes('E') && cycled[key].inherits.includes('OntologyEdge')
         ) {
           list.push({ name: key });
         }
@@ -99,57 +93,45 @@ export default class api {
 
       const now = new Date();
       const expiry = new Date(now);
-      expiry.setHours(now.getHours() + 8);
+      expiry.setHours(now.getHours() + CACHE_EXPIRY);
 
-      localStorage.setItem("edgeTypesExpiry", expiry.getTime());
-      localStorage.setItem("edgeTypes", JSON.stringify(list));
-      return promise.resolve(list);
+      localStorage.setItem('edgeTypesExpiry', expiry.getTime());
+      localStorage.setItem('edgeTypes', JSON.stringify(list));
+      return Promise.resolve(list);
     });
   }
 
   static getSources() {
-    const sources = localStorage.getItem("sources");
-    const sourcesExpiry = localStorage.getItem("sourcesExpiry");
+    const sources = localStorage.getItem('sources');
+    const sourcesExpiry = localStorage.getItem('sourcesExpiry');
     if (
-      !sources ||
-      (sources && sourcesExpiry && sourcesExpiry < Date.now().valueOf())
+      !sources || (sources && sourcesExpiry && sourcesExpiry < Date.now().valueOf())
     ) {
       return api.loadSources();
-    } else {
-      return Promise.resolve(JSON.parse(sources));
     }
+    return Promise.resolve(JSON.parse(sources));
   }
 
   static loadSources() {
-    return api.get("/sources").then(response => {
-      response = jc.retrocycle(response.result);
+    return api.get('/sources').then((response) => {
+      const cycled = jc.retrocycle(response.result);
       const list = [];
-      response.forEach(source => {
-        list.push(source);
-      });
+      cycled.forEach(source => list.push(source));
 
       const now = new Date();
       const expiry = new Date(now);
-      expiry.setHours(now.getHours() + 8);
+      expiry.setHours(now.getHours() + CACHE_EXPIRY);
 
-      localStorage.setItem("sourcesExpiry", expiry.getTime());
-      localStorage.setItem("sources", JSON.stringify(list));
+      localStorage.setItem('sourcesExpiry', expiry.getTime());
+      localStorage.setItem('sources', JSON.stringify(list));
 
-      return promise.resolve(list);
+      return Promise.resolve(list);
     });
   }
 
   static autoSearch(endpoint, property, value, limit) {
     return api.get(
-      "/" +
-        endpoint +
-        "?" +
-        property +
-        "=~" +
-        value +
-        "&limit=" +
-        limit +
-        "&neighbors=1"
+      `/${endpoint}?${property}=~${value}&limit=${limit}&neighbors=1`,
     );
   }
 }

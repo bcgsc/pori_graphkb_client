344cfec165a68bdb9cebd23926774ffa3a199bac
Isaac Beckie <ibeckie@bcgsc.ca>
2018-07-04 12:38:34 -0700

    Lint SVGNode, add exception to no-param-reassign for d3 simulation methods

--- src/components/SVGNode/SVGNode.js
+++ src/components/SVGNode/SVGNode.js
@@ -1,62 +1,90 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import './SVGNode.css';
 import * as d3 from 'd3';
-import ReactDOM from 'react-dom';
 
 class SVGNode extends PureComponent {
-    componentDidMount() {
-        let nodeElement = d3.select(ReactDOM.findDOMNode(this.refs['node' + this.props.node.index]));
-        nodeElement.call(d3.drag()
-            .on("start", element => this.dragstarted(this.props.node))
-            // .on("touchstart", element => dragstarted(node))
-        );
-    }
-    componentWillUnmount() {
-        let nodeElement = d3.select(ReactDOM.findDOMNode(this.refs['node' + this.props.node.index]));
-        nodeElement.call(d3.drag()
-            .on("start", null)
-        );
-    }
+  componentDidMount() {
+    const { node } = this.props;
+    const nodeElement = d3.select(this.node);
+    nodeElement.call(d3.drag()
+      .on('start', () => this.dragstarted(node)));
+  }
 
-    dragstarted(node) {
-        const simulation = this.props.simulation;
-        d3.event.sourceEvent.stopPropagation();
+  componentWillUnmount() {
+    const nodeElement = d3.select(this.node);
+    nodeElement.call(d3.drag()
+      .on('start', null));
+  }
 
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  dragstarted(node) {
+    const { simulation } = this.props;
+    d3.event.sourceEvent.stopPropagation();
 
-        d3.event
-            .on("drag", dragged)
-            // .on("touchmove", dragged)
-            .on("end", ended)
-        // .on("touchend", ended);
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
 
-        function dragged() {
-            node.fx = d3.event.x;
-            node.fy = d3.event.y;
-        }
-
-        function ended() {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            node.fx = null;
-            node.fy = null;
-        }
+    /* eslint-disable */
+    function dragged() {
+      node.fx = d3.event.x;
+      node.fy = d3.event.y;
     }
 
-    render() {
-
-        return (
-            <g ref={'node' + this.props.node.index} onClick={this.props.handleClick} transform={'translate(' + (this.props.node.x || 0) + ',' + (this.props.node.y || 0) + ')'}>
-                <circle className={this.props.expandable ? 'node-expandable' : 'node'}
-                    fill={this.props.color}
-                    cx={0}
-                    cy={0}
-                    r={this.props.r}
-                />
-                <text>
-                    <tspan className='node-name' dy={12}>{this.props.node.data.name}</tspan>
-                </text>
-
-            </g>
-        )
+    function ended() {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      node.fx = null;
+      node.fy = null;
     }
-} export default SVGNode
+    /* eslint-enable */
+
+    d3.event
+      .on('drag', dragged)
+      // .on("touchmove", dragged)
+      .on('end', ended);
+    // .on("touchend", ended);
+  }
+
+  render() {
+    const {
+      node,
+      handleClick,
+      expandable,
+      color,
+      r,
+    } = this.props;
+
+    return (
+      <g ref={(n) => { this.node = n; }} onClick={handleClick} transform={`translate(${(node.x || 0)},${(node.y || 0)})`}>
+        <circle
+          className={expandable ? 'node-expandable' : 'node'}
+          fill={color}
+          cx={0}
+          cy={0}
+          r={r}
+        />
+        <text>
+          <tspan className="node-name" dy={12}>
+            {node.data.name}
+          </tspan>
+        </text>
+      </g>
+    );
+  }
+}
+
+SVGNode.defaultProps = {
+  handleClick: null,
+  expandable: false,
+  color: '#000',
+  r: 4,
+};
+
+SVGNode.propTypes = {
+  node: PropTypes.object.isRequired,
+  handleClick: PropTypes.func,
+  expandable: PropTypes.bool,
+  color: PropTypes.string,
+  r: PropTypes.number,
+  simulation: PropTypes.object.isRequired,
+};
+
+export default SVGNode;

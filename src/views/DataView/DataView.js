ead617cb01306b425cbf779049acbadf8f44d3b0
Isaac Beckie <ibeckie@bcgsc.ca>
2018-07-04 10:24:02 -0700

    ESLint Dataview

--- src/views/DataView/DataView.js
+++ src/views/DataView/DataView.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import "./DataView.css";
-import api from "../../services/api";
-import { Route, Redirect } from "react-router-dom";
-import { Paper, Drawer, Button, IconButton } from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
-import GraphComponent from "../../components/GraphComponent/GraphComponent";
-import TableComponent from "../../components/TableComponent/TableComponent";
-import NodeFormComponent from "../../components/NodeFormComponent/NodeFormComponent";
-import * as jc from "json-cycle";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './DataView.css';
+import * as jc from 'json-cycle';
+import { Route, Redirect } from 'react-router-dom';
+import { Paper, Drawer, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import GraphComponent from '../../components/GraphComponent/GraphComponent';
+import TableComponent from '../../components/TableComponent/TableComponent';
+import NodeFormComponent from '../../components/NodeFormComponent/NodeFormComponent';
+import api from '../../services/api';
 
 class DataView extends Component {
   constructor(props) {
@@ -20,7 +21,6 @@ class DataView extends Component {
       displayed: [],
       selectedId: null,
       editing: false,
-      loginRedirect: false
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -34,42 +34,49 @@ class DataView extends Component {
   }
 
   componentDidMount() {
-    let dataMap = {};
-    let queryRedirect = this.state.queryRedirect;
-    let loginRedirect = this.state.loginRedirect;
+    const dataMap = {};
+    let { queryRedirect } = this.state;
+    const { loginRedirect } = this.state;
+    const { location } = this.props;
     api
-      .get("/diseases" + this.props.location.search)
-      .then(data => {
-        data = jc.retrocycle(data.result);
-        if (data.length === 0) queryRedirect = true;
-        data.forEach(ontologyTerm => {
-          dataMap[ontologyTerm["@rid"]] = ontologyTerm;
+      .get(`/diseases${location.search}`)
+      .then((data) => {
+        const cycled = jc.retrocycle(data.result);
+        if (cycled.length === 0) queryRedirect = true;
+        cycled.forEach((ontologyTerm) => {
+          dataMap[ontologyTerm['@rid']] = ontologyTerm;
         });
         this.setState({
           data: dataMap,
           selectedId: Object.keys(dataMap)[0],
           queryRedirect,
-          loginRedirect
+          loginRedirect,
         });
       })
-      .catch(error => this.setState({ loginRedirect: true }));
+      .catch((error) => {
+        if (error.status === 401) {
+          this.setState({ loginRedirect: true });
+        }
+      });
   }
 
   handleNodeAdd(node) {
     if (node.source.name) {
       const { data, displayed } = this.state;
-      data[node["@rid"]] = node;
-      if (displayed.indexOf(node["@rid"]) === -1) {
-        displayed.push(node["@rid"]);
+      data[node['@rid']] = node;
+      if (displayed.indexOf(node['@rid']) === -1) {
+        displayed.push(node['@rid']);
       }
       this.setState({ data, displayed });
     }
   }
+
   handleClick(rid) {
     this.setState({ selectedId: rid });
   }
+
   handleCheckbox(rid) {
-    const displayed = this.state.displayed;
+    const { displayed } = this.state;
     const i = displayed.indexOf(rid);
     if (i === -1) {
       displayed.push(rid);
@@ -78,51 +85,61 @@ class DataView extends Component {
     }
     this.setState({ displayed });
   }
+
   handleCheckAll(e) {
     let displayed;
+    const { data } = this.state;
     if (e.target.checked) {
-      displayed = Object.keys(this.state.data);
+      displayed = Object.keys(data);
     } else {
       displayed = [];
     }
     this.setState({ displayed });
   }
-  handleDrawerClose(e) {
+
+  handleDrawerClose() {
     this.setState({ editing: false });
   }
+
   handleNodeEditStart(rid) {
     this.setState({ selectedId: rid, editing: true });
   }
+
   handleNodeDelete(rid) {
-    const data = this.state.data;
-    console.log(data[rid])
+    const { data } = this.state;
     delete data[rid];
     this.setState({ data, editing: false });
   }
+
   handleNodeFinishEdit(node) {
-    const data = this.state.data;
+    const { data } = this.state;
     api
-      .get(
-        "/" +
-        node["@class"].toLowerCase() +
-        "s/" +
-        node["@rid"].slice(1) +
-        "?neighbors=3"
-      )
-      .then(response => {
-        data[node["@rid"]] = jc.retrocycle(response.result);
+      .get(`/${node['@class'].toLowerCase()}s/${node['@rid'].slice(1)}?neighbors=3`)
+      .then((response) => {
+        data[node['@rid']] = jc.retrocycle(response.result);
         this.setState({ data, editing: false });
       });
   }
 
   render() {
+    const {
+      editing,
+      selectedId,
+      data,
+      displayed,
+      queryRedirect,
+      loginRedirect,
+    } = this.state;
+
+    const { location } = this.props;
+
     const editDrawer = (
       <Drawer
         variant="persistent"
         anchor="right"
-        open={this.state.editing}
+        open={editing}
         classes={{
-          paper: "drawer-box-graph"
+          paper: 'drawer-box-graph',
         }}
         onClose={this.handleDrawerClose}
         SlideProps={{ unmountOnExit: true }}
@@ -134,7 +151,7 @@ class DataView extends Component {
             </IconButton>
           </div>
           <NodeFormComponent
-            selectedId={this.state.selectedId}
+            selectedId={selectedId}
             variant="edit"
             handleNodeFinishEdit={this.handleNodeFinishEdit}
             handleNodeDelete={this.handleNodeDelete}
@@ -146,35 +163,35 @@ class DataView extends Component {
     const GraphWithProps = () => (
       <GraphComponent
         handleNodeAdd={this.handleNodeAdd}
-        data={this.state.data}
-        search={this.props.location.search}
+        data={data}
+        search={location.search}
         handleClick={this.handleClick}
-        displayed={this.state.displayed}
-        selectedId={this.state.selectedId}
-        handleNodeEditStart={() =>
-          this.handleNodeEditStart(this.state.selectedId)
-        }
+        displayed={displayed}
+        selectedId={selectedId}
+        handleNodeEditStart={() => this.handleNodeEditStart(selectedId)}
       />
     );
     const TableWithProps = () => (
       <TableComponent
-        data={this.state.data}
-        selectedId={this.state.selectedId}
+        data={data}
+        selectedId={selectedId}
         handleClick={this.handleClick}
         handleCheckbox={this.handleCheckbox}
-        search={this.props.location.search}
-        displayed={this.state.displayed}
+        search={location.search}
+        displayed={displayed}
         handleCheckAll={this.handleCheckAll}
         handleNodeEditStart={this.handleNodeEditStart}
       />
     );
-    let dataView = () => {
-      if (this.state.queryRedirect)
-        return <Redirect push to={{ pathname: "/query" }} />;
-      if (this.state.loginRedirect)
-        return <Redirect push to={{ pathname: "/login" }} />;
+    const dataView = () => {
+      if (queryRedirect) {
+        return <Redirect push to={{ pathname: '/query' }} />;
+      }
+      if (loginRedirect) {
+        return <Redirect push to={{ pathname: '/login' }} />;
+      }
 
-      if (this.state.data) {
+      if (data) {
         return (
           <div className="data-view">
             <Route exact path="/data/table" render={TableWithProps} />
@@ -189,5 +206,9 @@ class DataView extends Component {
     return dataView();
   }
 }
+
+DataView.propTypes = {
+  location: PropTypes.object.isRequired,
+};
 
 export default DataView;

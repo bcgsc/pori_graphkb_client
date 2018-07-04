b044f6d15e4cbb04241242df9897990672c0f002
Isaac Beckie <isaacbeckie@gmail.com>
2018-07-04 15:14:16 -0700

    add log out button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter,
   Link,
@@ -12,9 +12,14 @@ import {
   createMuiTheme,
   MuiThemeProvider,
   IconButton,
+  Button,
+  Typography,
+  Menu,
+  MenuItem,
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import AddIcon from '@material-ui/icons/Add';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import QueryView from './views/QueryView/QueryView';
 import AdvancedQueryView from './views/AdvancedQueryView/AdvancedQueryView';
 import DataView from './views/DataView/DataView';
@@ -26,74 +31,133 @@ import auth from './services/auth';
 /**
  * Entry point to application. Handles routing set up as well as defining the app theme.
  */
-function App() {
-  const theme = createMuiTheme({
-    direction: 'ltr',
-    palette: {
-      primary: {
-        main: '#1F2B65',
-      },
-      secondary: {
-        light: '#009688',
-        main: '#00897b',
-      },
-      warn: {
-        main: '#d32f2f',
-      },
-    },
-  });
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+      loggedIn: !!auth.getToken(),
+    };
+    this.handleAuthenticate = this.handleAuthenticate.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
 
-  const addNodeForm = () => <NodeFormComponent variant="add" />;
+  handleAuthenticate() {
+    this.setState({ loggedIn: true });
+  }
 
-  const loggedInContent = (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/query" />
-      </Route>
-      <Route exact path="/query" component={QueryView} />
-      <Route path="/query/advanced" component={AdvancedQueryView} />
-      <Route path="/add" component={addNodeForm} />
-      <Route path="/data" component={DataView} />
-      <Route path="/error" component={ErrorView} />
-    </Switch>
-  );
+  handleOpen(e) {
+    this.setState({ anchorEl: e.currentTarget });
+  }
 
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <div className="App">
-          <AppBar position="static" className="banner">
-            <IconButton color="inherit" aria-label="open drawer">
-              <Link className="icon-link" to="/query">
-                <SearchIcon />
-              </Link>
-            </IconButton>
-            <IconButton color="inherit" aria-label="open drawer">
-              <Link className="icon-link" to="/add">
-                <AddIcon />
-              </Link>
-            </IconButton>
-          </AppBar>
-          <section className="content">
-            <div className="router-outlet">
-              <Switch>
-                <Route path="/login" component={LoginView} />
-                <Route
-                  path="/"
-                  render={() => {
-                    if (!auth.getToken()) {
-                      return <Redirect to="/login" />;
-                    }
-                    return loggedInContent;
-                  }}
-                />
-              </Switch>
-            </div>
-          </section>
-        </div>
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+  handleClose() {
+    this.setState({ anchorEl: null });
+  }
+
+  handleLogOut() {
+    auth.clearToken();
+    this.setState({ loggedIn: false }, this.handleClose);
+  }
+
+  render() {
+    const { anchorEl, loggedIn } = this.state;
+
+    const theme = createMuiTheme({
+      direction: 'ltr',
+      palette: {
+        primary: {
+          main: '#1F2B65',
+        },
+        secondary: {
+          light: '#009688',
+          main: '#00897b',
+        },
+        warn: {
+          main: '#d32f2f',
+        },
+      },
+    });
+
+    const addNodeForm = () => <NodeFormComponent variant="add" />;
+    const loginWithProps = () => (
+      <LoginView loggedIn={loggedIn} handleAuthenticate={this.handleAuthenticate} />
+    );
+
+    const loggedInContent = (
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/query" />
+        </Route>
+        <Route exact path="/query" component={QueryView} />
+        <Route path="/query/advanced" component={AdvancedQueryView} />
+        <Route path="/add" component={addNodeForm} />
+        <Route path="/data" component={DataView} />
+        <Route path="/error" component={ErrorView} />
+      </Switch>
+    );
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div className="App">
+            <AppBar position="static" className="banner">
+              <IconButton color="inherit" aria-label="open drawer">
+                <Link className="icon-link" to="/query">
+                  <SearchIcon />
+                </Link>
+              </IconButton>
+              <IconButton color="inherit" aria-label="open drawer">
+                <Link className="icon-link" to="/add">
+                  <AddIcon />
+                </Link>
+              </IconButton>
+
+              <div className="user-dropdown">
+                <Button
+                  classes={{ root: 'user-btn' }}
+                  onClick={this.handleOpen}
+                  size="small"
+                  disabled={!loggedIn}
+                >
+                  <AccountCircleIcon />
+                  <Typography variant="body2">
+                    {loggedIn ? auth.getUser() : 'Logged Out'}
+                  </Typography>
+                </Button>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={!!anchorEl}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleLogOut}>
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </div>
+            </AppBar>
+            <section className="content">
+              <div className="router-outlet">
+                <Switch>
+                  <Route path="/login" render={loginWithProps} />
+                  <Route
+                    path="/"
+                    render={() => {
+                      if (!loggedIn) {
+                        return <Redirect to="/login" />;
+                      }
+                      return loggedInContent;
+                    }}
+                  />
+                </Switch>
+              </div>
+            </section>
+          </div>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
--- src/views/LoginView/LoginView.js
+++ src/views/LoginView/LoginView.js
@@ -1,68 +1,87 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import "./LoginView.css";
-import { Button, Typography, TextField } from "@material-ui/core";
-import api from "../../services/api";
-import auth from "../../services/auth";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import './LoginView.css';
+import {
+  Button,
+  Typography,
+  TextField,
+} from '@material-ui/core';
+import api from '../../services/api';
+import auth from '../../services/auth';
 
 class LoginView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: "",
-      queryRedirect: false
+      username: '',
+      password: '',
+      invalid: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ [e.target.name]: e.target.value, invalid: false });
   }
 
   handleSubmit(e) {
     e.stopPropagation();
     e.preventDefault();
 
+    const { username, password } = this.state;
+    const { handleAuthenticate } = this.props;
+
     api
-      .post("/token", {
-        username: this.state.username,
-        password: this.state.password
+      .post('/token', {
+        username,
+        password,
       })
-      .then(response => {
+      .then((response) => {
         auth.loadToken(response.kbToken);
-        this.setState({ queryRedirect: true });
+        handleAuthenticate();
       })
-      .catch(error => {
-        if (error === 401) {
-          alert("Invalid Username or Password");
+      .catch((error) => {
+        if (error.status === 401) {
+          this.setState({ invalid: true });
         }
       });
   }
 
   render() {
-    if (this.state.queryRedirect) return <Redirect push to="/query" />;
+    const {
+      username,
+      password,
+      invalid,
+    } = this.state;
+
+    const { loggedIn } = this.props;
+
+    if (loggedIn) { return <Redirect push to="/query" />; }
+
     return (
       <div className="login-wrapper">
         <form className="login-form" onSubmit={this.handleSubmit}>
           <TextField
             className="login-input"
             name="username"
-            value={this.state.username}
+            value={username}
             onChange={this.handleChange}
             type="text"
             label="Username"
             required
+            error={invalid}
           />
           <TextField
             className="login-input"
             name="password"
-            value={this.state.password}
+            value={password}
             onChange={this.handleChange}
             type="password"
             label="Password"
             required
+            error={invalid}
           />
           <Button type="submit" color="primary" variant="raised">
             Login
@@ -70,9 +89,25 @@ class LoginView extends Component {
           <Typography variant="caption" id="caption">
             Log in with your BC GSC web credentials
           </Typography>
+          {invalid ? (
+            <Typography variant="subheading" id="invalid-meessage">
+              Invalid Username or Password
+            </Typography>
+          ) : null
+          }
         </form>
       </div>
     );
   }
 }
+
+LoginView.defaultProps = {
+  loggedIn: false,
+};
+
+LoginView.propTypes = {
+  handleAuthenticate: PropTypes.func.isRequired,
+  loggedIn: PropTypes.bool,
+};
+
 export default LoginView;

6bb84baf497e853ae9d84fcaa6137f6508891b1e
Isaac Beckie <isaacbeckie@gmail.com>
2018-07-05 13:36:39 -0700

    JSDocs for components

--- src/components/SVGNode/SVGNode.js
+++ src/components/SVGNode/SVGNode.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 import './SVGNode.css';
 import * as d3 from 'd3';
 
+/**
+ * Component used to display graph nodes and apply draggable behavior to them through d3.
+ */
 class SVGNode extends PureComponent {
+  /**
+   * Initializes node element and applies drag behavior to it.
+   */
   componentDidMount() {
     const { node } = this.props;
     const nodeElement = d3.select(this.node);
@@ -11,12 +17,19 @@ class SVGNode extends PureComponent {
       .on('start', () => this.dragstarted(node)));
   }
 
+  /**
+   * Removes d3 listener from object.
+   */
   componentWillUnmount() {
     const nodeElement = d3.select(this.node);
     nodeElement.call(d3.drag()
       .on('start', null));
   }
 
+  /**
+   * Applies drag behavior to node.
+   * @param {Object} node - node to be dragged.
+   */
   dragstarted(node) {
     const { simulation } = this.props;
     d3.event.sourceEvent.stopPropagation();
@@ -78,6 +91,14 @@ SVGNode.defaultProps = {
   r: 4,
 };
 
+/**
+ * @param {Object} node - Node to be rendered.
+ * @param {function} handleClick - Parent method on node click event.
+ * @param {bool} expandable - Expandable flag.
+ * @param {string} color - Color of node.
+ * @param {number} r - Node radius.
+ * @param {Object} simulation - parent simulation that node is a member of.
+ */
 SVGNode.propTypes = {
   node: PropTypes.object.isRequired,
   handleClick: PropTypes.func,

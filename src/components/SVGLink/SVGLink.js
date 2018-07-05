15e1465a7bec4e73f88b374586bf4600db4666cd
Isaac Beckie <ibeckie@bcgsc.ca>
2018-07-05 13:36:39 -0700

    JSDocs for components

--- src/components/SVGLink/SVGLink.js
+++ src/components/SVGLink/SVGLink.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './SVGLink.css';
 
-class SVGLink extends Component {
-  constructor(props) {
-    super(props);
+/**
+ * Display component for graph link objects.
+ * @param {Object} props.link - Graph link object to be displayed.
+ */
+function SVGLink(props) {
+  const { link } = props;
 
-    this.state = {
-      endMarker: 'url(#arrow)',
-      startMarker: (props.link.type === 'alias') ? 'url(#darrow' : '',
-    };
-  }
-
-  render() {
-    const { link } = this.props;
-    const { endMarker, startMarker } = this.state;
-    return (
-      <g>
-        <path
-          className="link"
-          d={`M${(link.source.x || 0)} ${(link.source.y || 0)}L${(link.target.x || 0)} ${(link.target.y || 0)}`}
-          markerEnd={endMarker}
-          markerStart={startMarker}
-        />
-      </g>
-    );
-  }
+  return (
+    <g>
+      <path
+        className="link"
+        d={`M${(link.source.x || 0)} ${(link.source.y || 0)}L${(link.target.x || 0)} ${(link.target.y || 0)}`}
+        markerEnd="url(#arrow)"
+        markerStart={(link.type === 'alias') ? 'url(#darrow' : ''}
+      />
+    </g>
+  );
 }
 
 SVGLink.propTypes = {

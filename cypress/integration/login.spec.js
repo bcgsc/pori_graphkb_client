2cf6345e737fbf70d633b661c86fa4741c62600d
Isaac Beckie <isaacbeckie@gmail.com>
2018-07-06 17:01:02 -0700

    Add cypress.io to project
    Create testing suites for login and query functions

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,71 @@
+import { credentials } from '../../config';
+
+function successfulLogIn() {
+  cy.get('input[name=username]').type(credentials.username);
+  cy.get('input[name=password]').type(`${credentials.password}{enter}`);
+}
+
+describe('Login Test', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('includes', '/login');
+    cy.contains('Log in with your BC GSC web credentials');
+  });
+
+  it('incorrectly logs in', () => {
+    cy.get('button[type=button]').each(($button) => {
+      cy.wrap($button).should('disabled');
+    });
+    cy.get('input[name=username]').type('invalid user');
+    cy.get('input[name=password]').type('invalid password{enter}');
+    cy.url().should('includes', '/login').should(() => {
+      /* eslint-disable */
+      expect(localStorage.getItem('kbToken')).to.be.null;
+      /* eslint-enable */
+    });
+    cy.contains('Invalid Username or Password');
+
+    cy.get('input[name=password]').type('invalid password');
+    cy.contains('Invalid Username or Password').should('not.exist');
+
+    cy.get('button[type=submit]').click();
+    cy.url().should('includes', '/login');
+    cy.contains('Invalid Username or Password');
+  });
+
+  it('correctly logs in', () => {
+    successfulLogIn();
+
+    cy.get('button[type=button]').each(($button) => {
+      cy.wrap($button).should('not.disabled');
+    });
+
+    cy.url().should('includes', '/query').should(() => {
+      /* eslint-disable */
+      expect(localStorage.getItem('kbToken')).to.not.be.null;
+      /* eslint-enable */
+    });
+    cy.contains(credentials.username);
+  });
+
+  it('logs out', () => {
+    successfulLogIn();
+
+    cy.get('div.user-dropdown button').click();
+    cy.get('ul li[role=menuitem]').contains('Logout').click();
+
+    cy.url().should('includes', '/login').should(() => {
+      /* eslint-disable */
+      expect(localStorage.getItem('kbToken')).to.be.null;
+      /* eslint-enable */
+    });
+
+    cy.get('button[type=button]').each(($button) => {
+      cy.wrap($button).should('disabled');
+    });
+    cy.url();
+    cy.contains('Log in with your BC GSC web credentials');
+    cy.contains('Invalid Username or Password').should('not.exist');
+    cy.contains('Logged Out');
+  });
+});

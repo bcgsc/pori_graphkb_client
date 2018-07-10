45643ad815973837592b719f31e80e26c5953197
Isaac Beckie <ibeckie@bcgsc.ca>
2018-07-10 11:43:16 -0700

    create download file component wrapper

---
+++ src/components/DownloadFileComponent/DownloadFileComponent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function DownloadFileComponent(props) {
+  const {
+    mediaType,
+    base64,
+    rawFileContent,
+    fileName,
+    children,
+  } = props;
+
+  const onClick = () => {
+    const uri = `data:${mediaType}${base64 ? `;${base64}` : null},${encodeURIComponent(rawFileContent)}`;
+
+    const link = document.createElement('a');
+    link.download = fileName;
+    link.href = uri;
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div
+      onClick={onClick}
+      onKeyUp={(e) => { if (e.keyCode === 13) onClick(); }}
+      role="button"
+      tabIndex={0}
+    >
+      {children}
+    </div>
+  );
+}
+
+DownloadFileComponent.propTypes = {
+  mediaType: PropTypes.string,
+  base64: PropTypes.string,
+  rawFileContent: PropTypes.string,
+  fileName: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+DownloadFileComponent.defaultProps = {
+  mediaType: 'text/plan;charset=US-ASCII',
+  base64: null,
+  rawFileContent: '',
+  fileName: 'download.txt',
+};
+
+export default DownloadFileComponent;

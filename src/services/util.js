41627164d18ce7796f1ca780a40e38ae779684f2
Isaac Beckie <isaacbeckie@gmail.com>
2018-07-11 12:03:17 -0700

    Format node details page. Implement expansion panels for nested object parameters, add formatting helper methods in util service.

--- src/services/util.js
+++ src/services/util.js
@@ -1,3 +1,7 @@
+
+const propOrder = ['sourceId', 'name'];
+
+
 /**
  * Handles miscellaneous tasks.
  */
@@ -24,5 +28,43 @@ export default class util {
     if (accstr.startsWith('@')) accstr = accstr.slice(1);
     accstr = accstr.charAt(0).toUpperCase() + accstr.slice(1);
     return accstr.replace(/[A-Z]/g, match => ` ${match}`);
+  }
+
+  /**
+   * Returns a representative field of a given object. Defaults to:
+   * sourceId, then name, then if neither are present, the first primitive
+   * type field in the object.
+   * @param {Object} obj - target data object.
+   */
+  static getPreview(obj) {
+    let preview;
+    propOrder.forEach((prop) => {
+      if (obj[prop]) {
+        if (!preview) {
+          preview = obj[prop];
+        }
+      }
+    });
+    if (!preview) {
+      const prop = Object.keys(obj).filter(key => typeof obj[key] !== 'object')[0];
+      preview = obj[prop];
+    }
+    return preview;
+  }
+
+  /**
+   * Formatter meant for edge types with given in the form:
+   * '['in' | 'out']_[edgeType]'.
+   *
+   *    Format string:  in_[edgeType] => has[edgeType]
+   *    out_[edgeType] => [edgeType]
+   *
+   * @param {string} str - string to be formatted.
+   */
+  static getEdgeLabel(str) {
+    const edgeType = str.split('_')[1];
+    return str.startsWith('in_')
+      ? `has${edgeType.slice(0, edgeType.length - 2)}`
+      : edgeType;
   }
 }

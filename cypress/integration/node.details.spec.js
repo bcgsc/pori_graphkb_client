d4c6943ddfc56249317e8c3132757e88a7a41d30
Isaac Beckie <isaacbeckie@gmail.com>
2018-07-16 09:39:58 -0700

    Add 'log:false' option to password typing commands in Cypress tests

--- cypress/integration/node.details.spec.js
+++ cypress/integration/node.details.spec.js
@@ -1,11 +1,9 @@
-import { credentials } from '../../config';
-
 describe('Node Detail View Test', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('includes', '/login');
-    cy.get('input[name=username]').type(credentials.username);
-    cy.get('input[name=password]').type(`${credentials.password}{enter}`);
+    cy.get('input[name=username]').type(Cypress.env('USER'));
+    cy.get('input[name=password]').type(`${Cypress.env('PASSWORD')}{enter}`, { log: false });
     cy.url().should('includes', '/query');
     cy.get('a[href="/query/advanced"]').children().click();
     cy.url().should('includes', '/query/advanced');

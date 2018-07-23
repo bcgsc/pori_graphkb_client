c03652faf441079aef89c3a1ef0e1977774775f0
Isaac Beckie <isaacbeckie@gmail.com>
2018-07-23 16:52:53 -0700

    add some graph tests for different edge indicators

--- cypress/integration/graph.spec.js
+++ cypress/integration/graph.spec.js
@@ -1,10 +1,15 @@
 
-function diseases() {
+function getClass(endpoint, params) {
   cy.contains('Advanced Search').click();
   cy.url().should('includes', '/query/advanced');
   cy.get('#class-adv').click();
-  cy.get('ul> li[data-value=Disease]').click();
-  cy.get('input[name=limit]').type('{backspace}{backspace}{backspace}');
+  cy.get(`ul> li[data-value=${endpoint}]`).click();
+  cy.get('input[name=limit]').type('{backspace}{backspace}');
+  if (Object.keys(params || {}).length !== 0) {
+    Object.keys(params).forEach((param) => {
+      cy.get(`input[name=${param}]`).type(params[[param]]);
+    });
+  }
   cy.get('#search-button').click();
   cy.get('table tbody tr:first input[type=checkbox]').click();
   cy.get('div.pag button.graph-btn').click();
@@ -20,15 +25,15 @@ describe('Graph View Test', () => {
   });
 
   it('Diseases actions ring', () => {
-    diseases();
+    getClass('Disease');
     cy.get('circle.node-expandable').click();
     cy.contains('(Details)').click();
     cy.contains('Properties:');
     cy.get('div.graph-close-drawer-btn button').click();
 
     cy.get('circle.node-expandable').click();
-    cy.contains('(Back)').click();
-    cy.contains('(Back)').should('not.exist');
+    cy.contains('(Close)').click();
+    cy.contains('(Close)').should('not.exist');
 
     let nodes = 1;
     cy.get('circle.node-expandable').click();
@@ -46,5 +51,15 @@ describe('Graph View Test', () => {
         expect(array.length).to.be.lessThan(nodes);
       });
     });
+  });
+
+  it.only('Features different edges', () => {
+    getClass('Feature', { name: 'a1bg-as1' });
+    cy.get('circle.node-expandable').click();
+    cy.contains('(Expand)').click();
+    cy.get('path.link:first').trigger('mouseover');
+    cy.contains('DeprecatedBy');
+    cy.get('path.link:first').trigger('mouseout', { force: true });
+    cy.contains('DeprecatedBy').should('not.exist');
   });
 });

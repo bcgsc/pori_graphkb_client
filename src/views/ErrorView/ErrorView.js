88c91fd0e835f2fe95c7eda667742e3acb3fd322
Isaac Beckie <ibeckie@bcgsc.ca>
2018-07-26 16:27:56 -0700

    refactor error view

--- src/views/ErrorView/ErrorView.js
+++ src/views/ErrorView/ErrorView.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ErrorView.css';
-import { Typography, Button } from '@material-ui/core';
+import { Typography, Button, Tooltip } from '@material-ui/core';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 
 /**
@@ -12,72 +12,92 @@ import AssignmentIcon from '@material-ui/icons/Assignment';
  * @param {Object} props.history - history property for the route and passed state.
  * @param {Object} props.history.location.state - passed state from originator of redirect.
  */
-function ErrorView(props) {
-  const { history } = props;
-  const { state } = history.location;
-  if (!state || (!state.status && !state.body)) history.push('/query');
-  const { status, body } = state;
+class ErrorView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tooltip: false,
+    };
+  }
 
-  const {
-    error,
-    message,
-    method,
-    name,
-    url,
-    stacktrace,
-  } = body;
+  render() {
+    const { tooltip } = this.state;
+    const { history } = this.props;
+    const { state } = history.location;
+    if (!state || (!state.status && !state.body)) history.push('/query');
+    const { status, body } = state;
 
-  const methodText = `${method ? `Method ${method}` : ''}${url ? `to url ${url}:` : ''}${message || ''}`;
+    const {
+      error,
+      message,
+      method,
+      name,
+      url,
+      stacktrace,
+    } = body;
 
-  return (
-    <div className="error-wrapper">
-      <Typography variant="display4" id="error-header">
-        Error
-      </Typography>
-      <div className="error-content">
-        <Typography variant="display2">
-          {`${status}: ${error || name}`}
+    const methodText = `${method ? `Method ${method}` : ''}${url ? `to url ${url}:` : ''}${message || ''}`;
+
+    return (
+      <div className="error-wrapper">
+        <Typography variant="display4" id="error-header">
+          Error
         </Typography>
-      </div>
+        <div className="error-content">
+          <Typography variant="display2">
+            {`${status}: ${error || name}`}
+          </Typography>
+        </div>
 
-      <div className="error-content">
-        <Typography variant="title">
-          {!error ? '' : name}
-        </Typography>
+        <div className="error-content">
+          <Typography variant="title">
+            {!error ? '' : name}
+          </Typography>
+        </div>
+        <div className="error-content">
+          <Typography variant="subheading">
+            {methodText}
+          </Typography>
+        </div>
+        <div id="spacer" />
+        <div className="error-content" id="return-link">
+          <Link to="/query">
+            <Button variant="raised" color="primary">
+              Back
+            </Button>
+          </Link>
+        </div>
+        {stacktrace
+          ? (
+            <div className="error-content stacktrace">
+              <Tooltip
+                title="Copied!"
+                disableHoverListener
+                open={tooltip}
+                onClose={() => this.setState({ tooltip: false })}
+              >
+                <CopyToClipboard text={stacktrace} id="copy-button">
+                  <Button
+                    variant="outlined"
+                    onClick={() => this.setState({ tooltip: true })}
+                  >
+                    Copy To Clipboard
+                    <AssignmentIcon />
+                  </Button>
+                </CopyToClipboard>
+              </Tooltip>
+              <Typography variant="subheading" id="stacktrace-text">
+                {stacktrace}
+              </Typography>
+            </div>) : null}
       </div>
-      <div className="error-content">
-        <Typography variant="subheading">
-          {methodText}
-        </Typography>
-      </div>
-      <div id="spacer" />
-      <div className="error-content" id="return-link">
-        <Link to="/query">
-          <Button variant="raised" color="primary">
-            Back
-          </Button>
-        </Link>
-      </div>
-      {stacktrace
-        ? (
-          <div className="error-content stacktrace">
-            <CopyToClipboard text={stacktrace} id="copy-button">
-              <Button variant="outlined">
-                Copy To Clipboard
-                <AssignmentIcon />
-              </Button>
-            </CopyToClipboard>
-            <Typography variant="subheading" id="stacktrace-text">
-              {stacktrace}
-            </Typography>
-          </div>) : null}
-    </div>
-  );
+    );
+  }
 }
 
 /**
- * @param {Object} history - Application routing history object.
- */
+* @param {Object} history - Application routing history object.
+  */
 ErrorView.propTypes = {
   history: PropTypes.object.isRequired,
 };

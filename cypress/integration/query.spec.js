166f8254f6597843d62f70db6080089874790b03
Isaac Beckie <ibeckie@bcgsc.ca>
2018-08-03 09:30:45 -0700

    cypress test update, add search button clicks in case enter key fails to submit

--- cypress/integration/query.spec.js
+++ cypress/integration/query.spec.js
@@ -5,6 +5,7 @@ describe('Query Page Test', () => {
     cy.url().should('includes', '/login');
     cy.get('input[name=username]').type(Cypress.env('USER'));
     cy.get('input[name=password]').type(`${Cypress.env('PASSWORD')}{enter}`, { log: false });
+    cy.get('#search-btn').click();
     cy.wait(500);
     cy.url().should('includes', '/query');
   });

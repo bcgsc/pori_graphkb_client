afbe807f9b0eb409dda323a29c7d48c783313b0f
Isaac Beckie <isaacbeckie@gmail.com>
2018-08-07 13:38:06 -0700

    KBDEV-130: Link labels appear statically when a property is selected in graph options panel

--- src/components/GraphLink/GraphLink.js
+++ src/components/GraphLink/GraphLink.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './GraphLink.css';
 
@@ -6,86 +6,57 @@ import './GraphLink.css';
  * Display component for graph link objects.
  * @param {Object} props.link - Graph link object to be displayed.
  */
-class GraphLink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayType: false,
-    };
-    this.handleDisplayTypeExit = this.handleDisplayTypeExit.bind(this);
-    this.handleDisplayTypeEnter = this.handleDisplayTypeEnter.bind(this);
+function GraphLink(props) {
+  const {
+    link,
+    detail,
+    labelKey,
+    color,
+  } = props;
+
+  const left = link.source.x < link.target.x;
+
+  let marker = '';
+  if (link.source !== link.target) {
+    marker = 'url(#endArrow)';
   }
 
-  /**
-   * Highlights link arrows and displays label along link path.
-   */
-  handleDisplayTypeEnter() {
-    this.setState({ displayType: true });
+  let label = '';
+  if (labelKey && labelKey.includes('.')) {
+    const keys = labelKey.split('.');
+    label = link.data[keys[0]][keys[1]];
+  } else if (labelKey) {
+    label = link.data[labelKey];
   }
 
-  /**
-   * Stops highlight effect and label.
-   */
-  handleDisplayTypeExit() {
-    this.setState({ displayType: false });
-  }
-
-  render() {
-    const {
-      link,
-      detail,
-      labelKey,
-      color,
-    } = this.props;
-
-    const { displayType } = this.state;
-    const left = link.source.x < link.target.x;
-
-    let marker = '';
-    if (link.source !== link.target) {
-      marker = 'url(#endArrow)';
-    }
-
-    let label = '';
-    if (labelKey && labelKey.includes('.')) {
-      const keys = labelKey.split('.');
-      label = link.data[keys[0]][keys[1]];
-    } else if (labelKey) {
-      label = link.data[labelKey];
-    }
-
-    return (
-      <g
-        onMouseEnter={this.handleDisplayTypeEnter}
-        onMouseLeave={this.handleDisplayTypeExit}
-      >
-        <path
-          className="link-widen"
-          d={`M${(link.source.x || 0)} ${(link.source.y || 0)}L${(link.target.x || 0)} ${(link.target.y || 0)}`}
-        />
-        <path
-          className="link"
-          id={`link${link.data['@rid']}`}
-          d={`M${(link.source.x || 0)} ${(link.source.y || 0)}L${(link.target.x || 0)} ${(link.target.y || 0)}`}
-          markerEnd={marker}
-          style={{ opacity: detail ? 0.4 : 0.7, strokeOpacity: detail ? 0.4 : 0.7 }}
-          fill={color}
-          stroke={color}
-        />
-        {displayType ? (
-          <text fill={color} opacity={detail ? 0.4 : 0.7}>
-            <textPath
-              href={`#link${link.data['@rid']}`}
-              startOffset="20%"
-              side={left ? 'left' : 'right'}
-            >
-              {label}
-            </textPath>
-          </text>
-        ) : null}
-      </g>
-    );
-  }
+  return (
+    <g>
+      <path
+        className="link-widen"
+        d={`M${(link.source.x || 0)} ${(link.source.y || 0)}L${(link.target.x || 0)} ${(link.target.y || 0)}`}
+      />
+      <path
+        className="link"
+        id={`link${link.data['@rid']}`}
+        d={`M${(link.source.x || 0)} ${(link.source.y || 0)}L${(link.target.x || 0)} ${(link.target.y || 0)}`}
+        markerEnd={marker}
+        style={{ opacity: detail ? 0.4 : 0.7, strokeOpacity: detail ? 0.4 : 0.7 }}
+        fill={color}
+        stroke={color}
+      />
+      {labelKey ? (
+        <text fill={color} opacity={detail ? 0.4 : 0.7}>
+          <textPath
+            href={`#link${link.data['@rid']}`}
+            startOffset="20%"
+            side={left ? 'left' : 'right'}
+          >
+            {label}
+          </textPath>
+        </text>
+      ) : null}
+    </g>
+  );
 }
 
 /**

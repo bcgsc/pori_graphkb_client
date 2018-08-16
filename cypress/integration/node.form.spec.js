16399036cf0c466511a289cc1af99899a31600fb
Isaac Beckie <ibeckie@bcgsc.ca>
2018-08-15 17:05:17 -0700

    add cypress tests for forms

--- cypress/integration/node.form.spec.js
+++ cypress/integration/node.form.spec.js
@@ -5,8 +5,6 @@ describe('Node Form Test', () => {
     cy.get('input[name=username]').type(Cypress.env('USER'));
     cy.get('input[name=password]').type(`${Cypress.env('PASSWORD')}{enter}`, { log: false });
     cy.url().should('includes', '/query');
-    cy.get('button.advanced-button').click();
-    cy.url().should('includes', '/query/advanced');
   });
 
   it('Form fields', () => {
@@ -19,22 +17,58 @@ describe('Node Form Test', () => {
     cy.get('#submit-btn').should('not.disabled');
 
     cy.get('input[name=subset]').type('cypress{enter}');
-    cy.get('ul.list li:first').contains('cypress');
-    cy.get('li.relationship-add-wrapper div div.resource-select:first').click();
-    cy.get('div[role=document]').scrollTo('bottom');
-    cy.get('li[data-value="#15:12"]').click();
-    cy.contains('test');
-    cy.get('#relationship-type').click().wait(100).click();
-    cy.contains('SubClassOf').click();
-    cy.contains('SubClassOf');
+    cy.get('ul div.subset-chip:first').contains('cypress');
+    cy.get('#relationship-add td div.resource-select').each((resourceSelect, i) => {
+      if (i === 1) {
+        cy.wrap(resourceSelect).click();
+        cy.get('div[role=document]');
+        cy.get('li[data-value="#15:12"]').click();
+        cy.contains('test');
+        cy.get('#relationship-type').click().wait(100).click();
+        cy.contains('SubClassOf').click();
+        cy.contains('SubClassOf');
 
-    cy.get('button[name=direction]').click();
-    cy.contains('SubClassOf').should('not.exist');
-    cy.contains('hasSubClass');
+        cy.get('button[name=direction]').click();
+        cy.contains('SubClassOf').should('not.exist');
+        cy.contains('hasSubClass');
 
-    cy.get('input[name=targetName]').type('angiosarcoma').wait(500);
+        cy.get('div.relationships-wrapper input[name=name]').type('angiosarcoma');
+        cy.get('div.droptions ul li:first').wait(1000).click({ force: true });
+        cy.get('#relationship-add td button[type=button]:first').click();
+        cy.get('tbody tr').then(list => expect(list.length).to.be.eq(2));
+      }
+    });
+  });
+
+  it('Submit test', () => {
+    cy.visit('/add');
+    cy.contains('Disease');
+    cy.get('#submit-btn').should('disabled');
+    cy.get('input[name=source][type=text]').type('test').wait(100);
     cy.get('div.droptions ul li:first').click();
-    cy.get('#relationship-add').click();
-    cy.get('div.relationship-item').should('exist');
+    cy.get('textarea[name=sourceId][type=text]').type('test').wait(100);
+    cy.get('#submit-btn').should('not.disabled');
+    cy.get('#submit-btn').click();
+    cy.contains('Completed!');
+  });
+
+  it('Edit test', () => {
+    cy.get('button.advanced-button').click();
+    cy.url().should('includes', '/query/advanced');
+    cy.get('input[name=source]').type('test');
+    cy.get('div.droptions li:first').click();
+    cy.get('#search-button').click();
+    cy.get('button.detail-btn:first').click();
+    cy.get('div.node-edit-btn button').click();
+    cy.url().should('includes', '/edit');
+    cy.get('textarea[name=sourceId]').invoke('text').should('not.empty');
+    cy.get('input[name=source]').should('not.have.value', '');
+    cy.get('#delete-btn').click();
+    cy.contains('Really Delete this Term?').should('exist');
+    cy.get('div[role=dialog] button:first').click();
+    cy.get('#submit-btn').click();
+    cy.contains('Completed!');
+    cy.get('div[role=alertdialog] button').click();
+    cy.url().should('includes', '/query');
   });
 });

fda97b9c9b7afab8fa428e3ceab118076dab3417
Isaac Beckie <isaacbeckie@gmail.com>
2018-08-31 14:59:10 -0700

    update api version. fix excludedprops validation

--- src/components/FormTemplater/FormTemplater.js
+++ src/components/FormTemplater/FormTemplater.js
@@ -57,88 +57,89 @@ class FormTemplater extends Component {
         linkedClass,
         description,
       } = property;
-      if (!excludedProps.includes(name)) { fields[name] = null; }
-      // Radio group component for boolean types.
-      if (type === 'boolean') {
-        fields.push(
-          <ListItem className="input-wrapper" key={name}>
-            <FormControl component="fieldset" required={mandatory}>
-              <FormLabel>
-                {util.antiCamelCase(name)}
-              </FormLabel>
-              <RadioGroup
-                name={name}
-                onChange={handleChange}
-                value={model[name].toString()}
-                style={{ flexDirection: 'row' }}
-              >
-                <FormControlLabel value="true" control={<Radio />} label="Yes" />
-                <FormControlLabel value="false" control={<Radio />} label="No" />
-              </RadioGroup>
-            </FormControl>
-          </ListItem>,
-        );
-      } else if (type === 'link') {
-        // If type is a link to another record, must find that record in the
-        // database and store its rid.
+      if (!excludedProps.includes(name)) {
+        // Radio group component for boolean types.
+        if (type === 'boolean') {
+          fields.push(
+            <ListItem className="input-wrapper" key={name}>
+              <FormControl component="fieldset" required={mandatory}>
+                <FormLabel>
+                  {util.antiCamelCase(name)}
+                </FormLabel>
+                <RadioGroup
+                  name={name}
+                  onChange={handleChange}
+                  value={model[name].toString()}
+                  style={{ flexDirection: 'row' }}
+                >
+                  <FormControlLabel value="true" control={<Radio />} label="Yes" />
+                  <FormControlLabel value="false" control={<Radio />} label="No" />
+                </RadioGroup>
+              </FormControl>
+            </ListItem>,
+          );
+        } else if (type === 'link') {
+          // If type is a link to another record, must find that record in the
+          // database and store its rid.
 
-        // Decide which endpoint to query.
-        let endpoint;
-        if (linkedClass) {
-          endpoint = linkedClass.route.slice(1);
-        }
+          // Decide which endpoint to query.
+          let endpoint;
+          if (linkedClass) {
+            endpoint = linkedClass.route.slice(1);
+          }
 
-        fields.push(
-          <ListItem key={name} style={{ display: 'block' }}>
-            <div>
-              <AutoSearchComponent
+          fields.push(
+            <ListItem key={name} style={{ display: 'block' }}>
+              <div>
+                <AutoSearchComponent
+                  value={model[name]}
+                  onChange={handleChange}
+                  name={name}
+                  label={util.antiCamelCase(name)}
+                  limit={30}
+                  endpoint={endpoint}
+                  required={mandatory}
+                  property={!linkedClass ? ['name', 'sourceId'] : undefined}
+                />
+              </div>
+            </ListItem>,
+          );// } else if (type === 'embedded') { } // ONCE isAbstract is ready.
+        } else {
+          // For text fields, apply some final changes for number inputs.
+          let t;
+          let step;
+          if (type === 'string') {
+            t = 'text';
+          } else if (type === 'integer' || type === 'long') {
+            t = 'number';
+            step = 1;
+          }
+
+          fields.push(
+            <ListItem className="input-wrapper" key={name}>
+              <TextField
+                label={util.antiCamelCase(name)}
                 value={model[name]}
                 onChange={handleChange}
+                className="text-input"
                 name={name}
-                label={util.antiCamelCase(name)}
-                limit={30}
-                endpoint={endpoint}
+                type={t || ''}
+                step={step || ''}
                 required={mandatory}
-                property={!linkedClass ? ['name', 'sourceId'] : undefined}
+                multiline={t === 'text'}
+                InputProps={{
+                  endAdornment: description && (
+                    <InputAdornment position="end">
+                      <Tooltip title={description}>
+                        <HelpIcon color="primary" />
+                      </Tooltip>
+                    </InputAdornment>
+                  ),
+                }}
               />
-            </div>
-          </ListItem>,
-        );// } else if (type === 'embedded') { } // ONCE isAbstract is ready.
-      } else {
-        // For text fields, apply some final changes for number inputs.
-        let t;
-        let step;
-        if (type === 'string') {
-          t = 'text';
-        } else if (type === 'integer' || type === 'long') {
-          t = 'number';
-          step = 1;
+            </ListItem>,
+          );
         }
-
-        fields.push(
-          <ListItem className="input-wrapper" key={name}>
-            <TextField
-              label={util.antiCamelCase(name)}
-              value={model[name]}
-              onChange={handleChange}
-              className="text-input"
-              name={name}
-              type={t || ''}
-              step={step || ''}
-              required={mandatory}
-              multiline={t === 'text'}
-              InputProps={{
-                endAdornment: description && (
-                  <InputAdornment position="end">
-                    <Tooltip title={description}>
-                      <HelpIcon color="primary" />
-                    </Tooltip>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </ListItem>,
-        );
       }
     });
     return fields;

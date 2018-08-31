bcd8df1b3555b7a1717657613cb29e177bdad1f6
Isaac Beckie <isaacbeckie@gmail.com>
2018-08-31 11:26:29 -0700

    add cypress admin test

--- cypress/integration/admin.page.spec.js
+++ cypress/integration/admin.page.spec.js
@@ -7,9 +7,26 @@ describe('Admin Page Test', () => {
     cy.url().should('includes', '/query');
   });
 
-  it('Open Admin Page', () => {
+  it('Admin Page', () => {
     cy.get('div.user-dropdown').click();
     cy.contains('Admin').click();
-    cy.url().includes('/admin');
+    cy.url().should('includes', '/admin');
+    cy.contains('Admin');
+    cy.contains('Users');
+    cy.contains('User Groups');
+    cy.get('div.admin-users table tbody tr').then((array) => {
+      const { length } = array;
+      cy.get('div.admin-users table thead tr th:first').click();
+      cy.get('div.admin-section-heading-btns button:first').contains(length);
+    });
+    cy.get('div.admin-user-groups div[role=button]').each((row, i) => {
+      if (i === 1) {
+        cy.wrap(row).click({ force: true });
+        cy.get('div.user-group-toolbar button:first').click({ force: true });
+        cy.get('input[placeholder="Enter Group Name"]').should('exist');
+      } else if (i !== 0) {
+        cy.wrap(row).click({ force: true });
+      }
+    });
   });
 });

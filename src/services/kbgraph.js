c3eb6fdc9ff90d27ec4ffbda3b29d52a25afdd02
Isaac Beckie <ibeckie@bcgsc.ca>
2018-09-20 17:43:28 -0700

    add jsdocs to propsmap

--- src/services/kbgraph.js
+++ src/services/kbgraph.js
@@ -9,29 +9,64 @@ const DEFAULT_LINK_VPROPS = [
 ];
 
 
+/**
+ * Class to define graph props maps, which keeps track of the different
+ * properties that graph objects can be colored by, and the different entries
+ * in of each.
+ */
 export default class PropsMap {
   constructor() {
     this.nodeProps = {};
     this.linkProps = {};
   }
 
+  /**
+   * Loads a node's properties into the propsmap.
+   * @param {Object} node - Ontology object (GraphNode.data).
+   * @param {Array<string>} validProps - List of valid ontology properties.
+   */
   loadNode(node, validProps = DEFAULT_NODE_VPROPS) {
     this._loadObj('node', node, validProps);
   }
 
+  /**
+   * Loads a link's properties into the propsmap.
+   * @param {Object} link - KB edge object.
+   * @param {Array<string>} validProps - List of valid edge properties.
+   */
   loadLink(link, validProps = DEFAULT_LINK_VPROPS) {
     this._loadObj('link', link, validProps);
   }
 
+  /**
+   * Updates the propsMap after a node has been removed from the graph.
+   * @param {Object} node - Ontology object
+   * @param {Array<Object>} nodes - Graph nodes list.
+   * @param {Array<string>} validProps - List of valid ontology properties.
+   */
   removeNode(node, nodes, validProps = DEFAULT_NODE_VPROPS) {
     this._removeObj('node', node, nodes, validProps);
   }
 
+  /**
+   * Updates the propsMap after a link has been removed from the graph.
+   * @param {Object} link - KB edge object.
+   * @param {Array<Object>} links - Graph links list.
+   * @param {Array<string>} validProps - List of valid edge properties.
+   */
   removeLink(link, links, validProps = DEFAULT_LINK_VPROPS) {
     this._removeObj('link', link, links, validProps);
   }
 
 
+  /**
+   * Updates propsMap after an object is removed.
+   * @param {string} type - Type of object: ['node', 'link'].
+   * @param {Object} graphObj - Removed object.
+   * @param {Array<Object>} graphObjs - Graph objects (of type 'type') list.
+   * @param {Array<string>} validProps - List of valid properties for object
+   * type.
+   */
   _removeObj(type, graphObj, graphObjs, validProps) {
     this[`${type}Props`] = {};
     graphObjs.forEach((g) => {
@@ -41,6 +76,13 @@ export default class PropsMap {
     });
   }
 
+  /**
+   * Loads a object's properties into the propsMap.
+   * @param {string} type - Type of object: ['node', 'link']
+   * @param {Object} graphObj - Loaded object.
+   * @param {Array<string>} validProps - List of valid properties for object
+   * type.
+   */
   _loadObj(type, graphObj, validProps) {
     const props = this[`${type}Props`];
     validProps.forEach((prop) => {

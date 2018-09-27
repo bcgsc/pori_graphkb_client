f20a73c0a0329b870db8dd8525f50632595f785a
Isaac Beckie <isaacbeckie@gmail.com>
2018-09-27 15:44:06 -0700

    format templater groups

--- src/components/FormTemplater/FormTemplater.js
+++ src/components/FormTemplater/FormTemplater.js
@@ -265,11 +265,11 @@ class FormTemplater extends Component {
           && !Object.values(completedpairs).some(g => g.includes(property.name))
         ) {
           fields.push((
-            <div className="form-templater-group-wrapper" key={key} id={key}>
+            <ListItem component="div" className="form-templater-group-wrapper" key={key} id={key}>
               <div className="form-templater-row-grid">
                 {pairs[key].map(k => formatFormField(sortedProps.find(p => p.name === k)))}
               </div>
-            </div>
+            </ListItem>
           ));
           completedpairs[key] = pairs[key].slice();
         }

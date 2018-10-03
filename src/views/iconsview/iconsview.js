96fbaa49522a00f0bdbd1a49ed80e2a22de6376a
Isaac Beckie <isaacbeckie@gmail.com>
2018-10-03 09:47:54 -0700

    Add icons mapping module. Closes KBDEV-217

--- src/views/iconsview/iconsview.js
+++ src/views/iconsview/iconsview.js
@@ -5,52 +5,9 @@ import {
   ListItemText,
   List,
 } from '@material-ui/core';
-import LabelIcon from '@material-ui/icons/Label';
-import LabelTwoToneIcon from '@material-ui/icons/LabelTwoTone';
-import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import LabelImportantIcon from '@material-ui/icons/LabelImportant';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import LinkIcon from '@material-ui/icons/Link';
-import ClassIcon from '@material-ui/icons/Class';
-import DescriptionIcon from '@material-ui/icons/Description';
-import ListAltIcon from '@material-ui/icons/ListAlt';
-import DateRangeIcon from '@material-ui/icons/DateRange';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
-import LocationCityIcon from '@material-ui/icons/LocationCity';
-import HelpIcon from '@material-ui/icons/Help';
-import BacteriaIcon from '../../icons/BacteriaIcon/BacteriaIcon';
-import DNAIcon from '../../icons/DNAIcon/DNAIcon';
-import FlaskIcon from '../../icons/FlaskIcon/FlaskIcon';
-import MoleculeIcon from '../../icons/MoleculeIcon/MoleculeIcon';
-import OpenBookIcon from '../../icons/OpenBookIcon/OpenBookIcon';
-import WWWIcon from '../../icons/WWWIcon/WWWIcon';
 import util from '../../services/util';
+import icons from '../../icons/icons';
 
-const PROP_TO_ICON = {
-  name: <LabelIcon />,
-  sourceId: <LabelImportantIcon />,
-  source: <BookmarkIcon />,
-  description: <DescriptionIcon />,
-  subsets: <ListAltIcon />,
-  longName: <LabelTwoToneIcon />,
-  sourceIdVersion: <LocalOfferIcon />,
-  biotype: <BacteriaIcon />,
-  journalName: <OpenBookIcon />,
-  year: <DateRangeIcon />,
-  startYear: <DateRangeIcon />,
-  endYear: <DateRangeIcon />,
-  city: <LocationCityIcon />,
-  country: <LocationOnIcon />,
-  url: <WWWIcon />,
-  dependency: <AssignmentOutlinedIcon />,
-  '@class': <ClassIcon />,
-  molecularFormula: <MoleculeIcon />,
-  mechanismOfAction: <FlaskIcon />,
-  help: <HelpIcon />,
-  variant: <DNAIcon />,
-};
 
 /**
  * Feedback page
@@ -58,7 +15,7 @@ const PROP_TO_ICON = {
 function iconsview() {
   return (
     <List style={{ columnCount: 3 }}>
-      {Object.entries(PROP_TO_ICON).map(pair => (
+      {icons.getAllIcons().map(pair => (
         <ListItem key={pair[0]} style={{ display: 'inline-flex' }}>
           <ListItemIcon>
             <div>
@@ -68,22 +25,6 @@ function iconsview() {
           <ListItemText primary={util.antiCamelCase(pair[0])} />
         </ListItem>
       ))}
-      <ListItem style={{ display: 'inline-flex' }}>
-        <ListItemIcon>
-          <div>
-            <LinkIcon />
-          </div>
-        </ListItemIcon>
-        <ListItemText primary="Edges" />
-      </ListItem>
-      <ListItem style={{ display: 'inline-flex' }}>
-        <ListItemIcon>
-          <div>
-            <AssignmentIcon />
-          </div>
-        </ListItemIcon>
-        <ListItemText primary="Default" />
-      </ListItem>
     </List>
   );
 }

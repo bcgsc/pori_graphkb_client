59e4d26ce238232f7b50145f3059bbf0bbf4e6cc
Isaac Beckie <ibeckie@bcgsc.ca>
2018-10-03 15:10:25 -0700

    add some skeleton code

--- src/components/AutoSearchComponent/AutoSearchComponent.test.js
+++ src/components/AutoSearchComponent/AutoSearchComponent.test.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { spy } from 'sinon';
+import { TextField } from '@material-ui/core';
 import AutoSearchComponent from './AutoSearchComponent';
 
 spy(AutoSearchComponent.prototype, 'componentDidMount');
 
-describe('<Foo />', () => {
-  it('calls componentDidMount', () => {
-    mount(<AutoSearchComponent />);
-    expect(AutoSearchComponent.prototype.componentDidMount).to.have.property('callCount', 1);
+describe('<AutoSearchComponent />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<AutoSearchComponent />);
+  });
+
+  it('structure', () => {
+    expect(wrapper.type()).to.equal('div');
+    expect(wrapper.children().type()).to.equal('div');
+    expect(wrapper.find('.autosearch-popper-node').children().type()).to.equal(TextField);
   });
 });

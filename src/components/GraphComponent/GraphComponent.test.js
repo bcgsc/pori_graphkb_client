a6add86c5806b1133434953e3d4151c796255d81
Isaac Beckie <isaacbeckie@gmail.com>
2018-10-09 16:01:39 -0700

    add displayed prop in graphcomponent prop definitions

--- src/components/GraphComponent/GraphComponent.test.js
+++ src/components/GraphComponent/GraphComponent.test.js
@@ -6,18 +6,54 @@ import GraphComponent from './GraphComponent';
 
 spy(GraphComponent.prototype, 'componentDidMount');
 
-describe('<FormTemplater />', () => {
+const mockData = {
+  '#1': {
+    '@rid': '#1',
+    name: 'test one',
+    sourceId: 'test-1',
+  },
+  '#2': {
+    '@rid': '#2',
+    name: 'test two',
+    sourceId: 'test-2',
+    source: {
+      name: 'test source',
+    },
+  },
+  '#3': {
+    '@rid': '#3',
+    name: 'test three',
+    sourceId: 'test-3',
+  },
+};
+
+
+describe('<GraphComponent />', () => {
   let wrapper;
   it('init', () => {
     wrapper = mount(
       <GraphComponent
         data={{}}
-        handleDetailDrawerOpen={() => {}}
-        handleDetailDrawerClose={() => {}}
-        handleTableRedirect={() => {}}
-        handleNewColumns={() => {}}
+        handleDetailDrawerOpen={() => { }}
+        handleDetailDrawerClose={() => { }}
+        handleTableRedirect={() => { }}
+        handleNewColumns={() => { }}
       />,
     );
     expect(GraphComponent.prototype.componentDidMount).to.have.property('callCount', 1);
+  });
+
+  it('', () => {
+    wrapper = mount(
+      <GraphComponent
+        data={{ mockData }}
+        handleDetailDrawerOpen={() => { }}
+        handleDetailDrawerClose={() => { }}
+        handleTableRedirect={() => { }}
+        handleNewColumns={() => { }}
+        displayed={['#1', '#2', '#3']}
+      />,
+    );
+    console.log(wrapper);
   });
 });

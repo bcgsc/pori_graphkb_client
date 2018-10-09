f58e8f709bafb20bfb131eade8924b09ddb72c77
Isaac Beckie <ibeckie@bcgsc.ca>
2018-10-09 11:42:18 -0700

    add setup files for jest and enzyme, add detaildrawer test

---
+++ config/jest/browserMock.js
@@ -0,0 +1,20 @@
+// browser mocks
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem: key => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});

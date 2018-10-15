8a527777a0c323e27079867d85490f2e7340b21b
Isaac Beckie <isaacbeckie@gmail.com>
2018-10-15 16:05:54 -0700

    add more formtemplater tests

--- src/components/FormTemplater/FormTemplater.test.js
+++ src/components/FormTemplater/FormTemplater.test.js
@@ -13,7 +13,20 @@ describe('<FormTemplater />', () => {
     },
     otherProp: {
       name: 'otherProp',
-      type: 'number',
+      type: 'integer',
+    },
+    floatProp: {
+      name: 'floatProp',
+      type: 'long',
+    },
+    boolProp: {
+      name: 'bool monkey madness',
+      type: 'boolean',
+    },
+    choiceProp: {
+      name: 'choices',
+      type: 'string',
+      choices: ['choice one', 'blargh', 'node js interactive', 'sonic'],
     },
     linkProp: {
       name: 'linkProp',
@@ -27,7 +40,7 @@ describe('<FormTemplater />', () => {
     embeddedProp: {
       name: 'embeddedProp',
       type: 'embedded',
-      linkedClass: { name: 'V', route: '/v' },
+      linkedClass: { name: 'V', route: '/V' },
     },
   };
   const mockSchema = {
@@ -36,6 +49,12 @@ describe('<FormTemplater />', () => {
       properties: mockClass,
     },
     V: { name: 'V', properties: {} },
+    child: {
+      name: 'child',
+      properties: {},
+      inherits: ['V'],
+      route: '/child',
+    },
   };
 
   const mockModel = {
@@ -71,14 +90,19 @@ describe('<FormTemplater />', () => {
       .forEach(child => expect(child.children().props().component).to.equal('div'));
   });
 
-  it('fleshed out model', () => {
+  it('grouping', () => {
     wrapper = mount(
       <FormTemplater
         schema={mockSchema}
         model={mockModel}
         kbClass={mockClass}
         onChange={() => { }}
+        onClassChange={() => { }}
+        fieldComponent="div"
+        pairs={{ pair1: ['linkProp', 'boolProp'] }}
       />,
     );
+
+    expect(wrapper.find('.form-templater-group-wrapper'));
   });
 });

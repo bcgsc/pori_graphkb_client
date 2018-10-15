902bbd5e0a57affe5eea135f9268111257e75137
Isaac Beckie <isaacbeckie@gmail.com>
2018-10-15 14:24:11 -0700

    add more test cases to autosearch component

--- src/components/AutoSearchComponent/AutoSearchComponent.test.js
+++ src/components/AutoSearchComponent/AutoSearchComponent.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
+import { spy } from 'sinon';
 import { TextField } from '@material-ui/core';
 import Downshift from 'downshift';
 import AutoSearchComponent from './AutoSearchComponent';
+
+spy(AutoSearchComponent.prototype, 'refreshOptions');
 
 describe('<AutoSearchComponent />', () => {
   let wrapper;
@@ -27,5 +30,31 @@ describe('<AutoSearchComponent />', () => {
     expect(wrapper.find('input').props().placeholder).to.equal('testPlaceHolder');
     expect(wrapper.find('label').text()).to.equal('testLabel');
     expect(wrapper.find('input').props().disabled).to.equal(true);
+  });
+
+  it('focusing', () => {
+    wrapper = mount((
+      <AutoSearchComponent />
+    ));
+    wrapper.simulate('click');
+    const focusedEl = document.activeElement;
+    expect(wrapper.find('input').matchesElement(focusedEl));
+  });
+
+  it('input events', () => {
+    // Mocks
+    const onChange = jest.fn();
+    AutoSearchComponent.prototype.callApi = jest.fn();
+
+    wrapper = mount((
+      <AutoSearchComponent
+        onChange={onChange}
+      />
+    ));
+    wrapper.find('input').simulate('change');
+    wrapper.find('input').simulate('keyup');
+    wrapper.find('input').simulate('blur');
+    expect(onChange.mock.calls.length).to.be.gt(0);
+    expect(AutoSearchComponent.prototype.refreshOptions).to.have.property('callCount', 1);
   });
 });

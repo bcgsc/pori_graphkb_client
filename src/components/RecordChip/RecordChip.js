1d303d6783b4728ef90a90628f7333ec963cde62
Isaac Beckie <ibeckie@bcgsc.ca>
2018-11-07 18:49:54 -0800

    add popover to record chip to view short preview of selected record

--- src/components/RecordChip/RecordChip.js
+++ src/components/RecordChip/RecordChip.js
@@ -1,29 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './RecordChip.css';
 import {
   Chip,
   Avatar,
+  Popover,
+  Typography,
+  Divider,
+  Card,
+  CardContent,
 } from '@material-ui/core';
 import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
 
-function RecordChip(props) {
-  const {
-    ...other
-  } = props;
-  let className = 'record-chip-root';
-  if (other.className) {
-    className = `${className} ${other.className}`;
+class RecordChip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+    };
+    this.handlePopoverOpen = this.handlePopoverOpen.bind(this);
+    this.handlePopoverClose = this.handlePopoverClose.bind(this);
   }
 
-  return (
-    <Chip
-      {...other}
-      className={className}
-      clickable
-      avatar={<Avatar><AssignmentOutlinedIcon /></Avatar>}
-      variant="outlined"
-      color="primary"
-    />
-  );
+  handlePopoverClose() {
+    this.setState({ anchorEl: null });
+  }
+
+  handlePopoverOpen(e) {
+    this.setState({ anchorEl: e.currentTarget });
+  }
+
+  render() {
+    const {
+      record,
+      ...other
+    } = this.props;
+    const { anchorEl } = this.state;
+
+    let className = 'record-chip-root';
+    if (other.className) {
+      className = `${className} ${other.className}`;
+    }
+
+    return (
+      <React.Fragment>
+        <Popover
+          open={!!anchorEl}
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          transformOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          onClose={this.handlePopoverClose}
+        >
+          <Card>
+            <CardContent className="record-chip-panel">
+              <Typography variant="h6" gutterBottom>{record.getPreview()}</Typography>
+              <Divider />
+              <div className="record-chip-panel-row">
+                <Typography variant="subtitle1">@class</Typography>
+                <Typography variant="subtitle2">{record['@class']}</Typography>
+              </div>
+              <div className="record-chip-panel-row">
+                <Typography variant="subtitle1">@rid</Typography>
+                <Typography variant="subtitle2">{record.getId()}</Typography>
+              </div>
+              {record.source && (
+                <div className="record-chip-panel-row">
+                  <Typography variant="subtitle1">Source</Typography>
+                  <Typography variant="subtitle2">{record.source && record.source.name}</Typography>
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </Popover>
+        <Chip
+          {...other}
+          label={record.getPreview()}
+          className={className}
+          clickable
+          avatar={<Avatar><AssignmentOutlinedIcon /></Avatar>}
+          variant="outlined"
+          color="primary"
+          onClick={this.handlePopoverOpen}
+        />
+      </React.Fragment>
+    );
+  }
 }
 export default RecordChip;

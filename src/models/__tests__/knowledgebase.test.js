335d1080f318f23f93d2fce34e0920b03151c8b8
Isaac Beckie <isaacbeckie@gmail.com>
2018-11-08 13:37:11 -0800

    update tests to reflect new parser version

--- src/models/__tests__/knowledgebase.test.js
+++ src/models/__tests__/knowledgebase.test.js
@@ -176,7 +176,7 @@ describe('ontology methods test', () => {
       },
       untemplatedSeq: 'a',
     }, mockSchema, true);
-    expect(testPositionalVariant.getPreview()).to.eq('kras:g.1del');
+    expect(testPositionalVariant.getPreview().toLowerCase()).to.eq('kras:g.1del');
     expect(Position.getIdentifiers()).to.deep.eq(['@class', 'pos', 'refAA', 'arm', 'majorBand', 'minorBand']);
 
     const testBadRecord = new Source({ anythingElse: 'pass' });

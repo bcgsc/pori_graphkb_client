35b0d13d1107591472871435aa5be58119727b72
Isaac Beckie <isaacbeckie@gmail.com>
2018-12-05 12:09:19 -0800

    update unit tests

--- src/components/__tests__/CodeInput.test.js
+++ src/components/__tests__/CodeInput.test.js
@@ -16,4 +16,94 @@ describe('<CodeInput />', () => {
     wrapper.update();
     expect(wrapper.find('textarea.a-match')).to.have.lengthOf(1);
   });
+
+  it('handles TAB keypresses properly', () => {
+    wrapper.setProps({ value: '', onChange: jest.fn() });
+    wrapper.update();
+    wrapper.find('#typeTextArea').first().simulate('keydown');
+    wrapper
+      .find('#typeTextArea')
+      .first()
+      .simulate('keydown',
+        {
+          keyCode: 9,
+          target: { value: '' },
+          selectionStart: 0,
+          selectionEnd: 0,
+        });
+    expect(wrapper.props().onChange.mock.calls.length).to.eq(1);
+
+    wrapper
+      .find('#typeTextArea')
+      .first()
+      .simulate('keydown',
+        {
+          keyCode: 9,
+          shiftKey: true,
+          target: { value: '   ' },
+          selectionStart: 3,
+          selectionEnd: 3,
+        });
+    setTimeout(() => expect(wrapper.props().onChange.mock.calls.length).to.eq(2), 0);
+  });
+
+  it('handles BACKSPACE keypresses properly', () => {
+    wrapper.setProps({ value: '', onChange: jest.fn() });
+    wrapper.update();
+    wrapper
+      .find('#typeTextArea')
+      .first()
+      .simulate('keydown',
+        {
+          keyCode: 8,
+          ctrlKey: true,
+          target: { value: 'f   ' },
+          selectionStart: 2,
+          selectionEnd: 2,
+        });
+    wrapper
+      .find('#typeTextArea')
+      .first()
+      .simulate('keydown',
+        {
+          keyCode: 8,
+          ctrlKey: true,
+          target: { value: '' },
+          selectionStart: 0,
+          selectionEnd: 0,
+        });
+  });
+
+  it('handles DELETE keypresses properly', () => {
+    wrapper.setProps({ value: '', onChange: jest.fn() });
+    wrapper.update();
+    wrapper
+      .find('#typeTextArea')
+      .first()
+      .simulate('keydown',
+        {
+          keyCode: 46,
+          ctrlKey: true,
+          target: { value: '   ' },
+          selectionStart: 0,
+          selectionEnd: 0,
+        });
+    expect(wrapper.props().onChange.mock.calls.length).to.eq(1);
+    wrapper
+      .find('#typeTextArea')
+      .first()
+      .simulate('keydown',
+        {
+          keyCode: 46,
+          ctrlKey: true,
+          target: { value: 'a' },
+          selectionStart: 0,
+          selectionEnd: 0,
+        });
+    expect(wrapper.props().onChange.mock.calls.length).to.eq(1);
+  });
+
+  it('scrolls without crashing', () => {
+    wrapper.find('#typeTextArea').first().simulate('scroll');
+  });
 });

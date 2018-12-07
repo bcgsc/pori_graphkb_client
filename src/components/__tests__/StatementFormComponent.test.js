bb4656a7ac781c3c45551f0d95037c97bf4d10fa
Isaac Beckie <ibeckie@bcgsc.ca>
2018-12-07 13:38:07 -0800

    update schema package and tests

--- src/components/__tests__/StatementFormComponent.test.js
+++ src/components/__tests__/StatementFormComponent.test.js
@@ -17,7 +17,7 @@ const testSchema = new Schema({
   },
   E: {
     subclasses: [
-      { name: 'Implies' },
+      { name: 'ImpliedBy' },
       { name: 'SupportedBy' },
     ],
   },
@@ -114,8 +114,8 @@ const validNode = {
     in: { '@rid': '#1' },
     out: { '@rid': '#2' },
   }],
-  in_Implies: [{
-    '@class': 'Implies',
+  in_ImpliedBy: [{
+    '@class': 'ImpliedBy',
     in: { '@rid': '#1' },
     out: { '@rid': '#2' },
   }],
@@ -178,7 +178,7 @@ describe('<StatementFormComponent />', () => {
           '@class': 'SupportedBy',
         },
         {
-          '@class': 'Implies',
+          '@class': 'ImpliedBy',
         },
       ],
     });

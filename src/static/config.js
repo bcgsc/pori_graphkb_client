668cc1160d316b04b4226ce5972f4625883d5191
creisle <caralynreisle@gmail.com>
2018-12-26 20:49:09 -0800

    Use process.env prefix. It is required for dev server

--- src/static/config.js
+++ src/static/config.js
@@ -1,34 +1,24 @@
-const {
-  NODE_ENV,
-  KEYCLOAK_REALM,
-  KEYCLOAK_CLIENT_ID,
-  KEYCLOAK_URL,
-  KEYCLOAK_ROLE,
-  API_BASE_URL,
-  DISABLE_AUTH,
-} = process.env;
-
-const DEFAULT_URL = NODE_ENV === 'local'
+const DEFAULT_URL = process.env.NODE_ENV === 'local'
   ? 'http://localhost:8080/api'
   : 'http://kbapi01:8080/api';
 
 
 export default {
-  DISABLE_AUTH: (NODE_ENV === 'local'
+  DISABLE_AUTH: (process.env.NODE_ENV === 'local'
     ? true
-    : DISABLE_AUTH === '1'
+    : process.env.DISABLE_AUTH === '1'
   ),
-  API_BASE_URL: API_BASE_URL || DEFAULT_URL,
+  API_BASE_URL: process.env.API_BASE_URL || DEFAULT_URL,
   KEYS: {
     KB_TOKEN: 'kbToken',
     KEYCLOAK_TOKEN: 'kcToken',
     GRAPH_OBJECTS: 'graphObjects',
   },
   KEYCLOAK: {
-    REALM: KEYCLOAK_REALM || 'TestKB', // TODO: Migrate over to production keycloak realm (will probably be something like "GSC")s
-    CLIENT_ID: KEYCLOAK_CLIENT_ID || 'GraphKB',
-    URL: KEYCLOAK_URL || 'http://ga4ghdev01.bcgsc.ca:8080/auth',
-    GRAPHKB_ROLE: KEYCLOAK_ROLE || 'GraphKB',
+    REALM: process.env.KEYCLOAK_REALM || 'TestKB', // TODO: Migrate over to production keycloak realm (will probably be something like "GSC")s
+    CLIENT_ID: process.env.KEYCLOAK_CLIENT_ID || 'GraphKB',
+    URL: process.env.KEYCLOAK_URL || 'http://ga4ghdev01.bcgsc.ca:8080/auth',
+    GRAPHKB_ROLE: process.env.KEYCLOAK_ROLE || 'GraphKB',
   },
   FEEDBACK_EMAIL: 'graphkb@bcgsc.ca',
   FEEDBACK_HIPCHAT: '@IsaacBeckie',

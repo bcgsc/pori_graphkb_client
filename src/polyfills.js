651f3df150f0691e0f12dfa985300501bbf9f6ad
creisle <caralynreisle@gmail.com>
2018-12-29 16:14:19 -0800

    Move polyfills to production requirement not dev

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,6 @@
-'use strict';
+import '@babel/polyfill';
+import 'whatwg-fetch'; // fetch() polyfill for making API calls.
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
@@ -7,9 +9,6 @@ if (typeof Promise === 'undefined') {
   require('promise/lib/rejection-tracking').enable();
   window.Promise = require('promise/lib/es6-extensions.js');
 }
-
-// fetch() polyfill for making API calls.
-require('whatwg-fetch');
 
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.

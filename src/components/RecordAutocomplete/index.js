2130a7d87814a07781b4b1d916308c7a9ada201b
Caralyn Reisle <creisle@bcgsc.ca>
2019-03-04 12:40:16 -0800

    Do not async reselect for short lists
    - When the entire list of records is small, fetch the whole thing and
      store instead of re-calling to the API with keywords
    - Use a regular select component instead of an async one
    
    Addresses: KBDEV-401

--- src/components/RecordAutocomplete/index.js
+++ src/components/RecordAutocomplete/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 import AsyncSelect from 'react-select/lib/Async';
 import debounce from 'debounce-promise';
 import {
@@ -27,19 +28,24 @@ import './index.scss';
   * Autocomplete dropdown component for inputs which take 1 or multiple records as input
   *
   * @property {object} props
-  * @property {object} props.components components to be passed to react-select
-  * @property {object} props.DetailChipProps properties to be applied to the DetailChip
   * @property {boolean} props.disabled flag to indicate this input is disabled
   * @property {boolean} props.isMulti flag to indicate this field accepts multiple records
+  * @property {boolean} props.required flag to indicate that this field must be filled
+  * @property {function} props.getOptionKey function to be used in generating a key for comparing options to check if equal
+  * @property {function} props.getOptionLabel function to get the string representation of the option
   * @property {function} props.itemToString function to convert option objects to display label
-  * @property {string} props.label the label for this form field
-  * @property {Number} props.minSearchLength the minimum length of characters required before the async options handler is called
-  * @property {string} props.name the name of the field, used for propgating events
   * @property {function} props.onChange the parent handler function
-  * @property {string} props.placeholder the text placeholder for the search box
-  * @property {searchHandler} props.searchHandler the function to create the async options call
-  * @property {object|Array.<object>} props.value the initial selected value(s)
   * @property {Number} props.debounceMs the ms to use in setting the debounce on calling getting options (id 0 then no debounce is set)
+  * @property {Number} props.minSearchLength the minimum length of characters required before the async options handler is called
+  * @property {object} props.components components to be passed to react-select
+  * @property {object} props.DetailChipProps properties to be applied to the DetailChip
+  * @property {object|Array.<object>} props.value the initial selected value(s)
+  * @property {searchHandler} props.searchHandler the function to create the async options call
+  * @property {string} props.className Additional css class name to use on the main select component
+  * @property {string} props.errorText Error message
+  * @property {string} props.label the label for this form field
+  * @property {string} props.name the name of the field, used for propgating events
+  * @property {string} props.placeholder the text placeholder for the search box
   */
 class RecordAutocomplete extends React.Component {
   static propTypes = {
@@ -96,6 +102,7 @@ class RecordAutocomplete extends React.Component {
     this.state = {
       selected: value,
       helperText: false,
+      initialOptions: [],
     };
     this.controller = null; // store the request/abort object to handle the async option call
     if (debounceMs < 0) {
@@ -103,6 +110,15 @@ class RecordAutocomplete extends React.Component {
     } else {
       // wrap in a debouncer to avoid spamming the api
       this.getOptions = debounce(this.getOptions.bind(this), debounceMs);
+    }
+  }
+
+  async componentDidMount() {
+    const { searchHandler, minSearchLength } = this.props;
+    if (minSearchLength === 0) {
+      this.controller = searchHandler('');
+      const initialOptions = await this.controller.request();
+      this.setState({ initialOptions });
     }
   }
 
@@ -187,15 +203,29 @@ class RecordAutocomplete extends React.Component {
     const {
       helperText,
       selected,
+      initialOptions,
     } = this.state;
+
+    let BaseSelectComponent;
+    let uniqueProps;
+
+    if (minSearchLength === 0) {
+      BaseSelectComponent = Select;
+      uniqueProps = { options: initialOptions };
+    } else {
+      // Async requests
+      BaseSelectComponent = AsyncSelect;
+      uniqueProps = {
+        loadOptions: this.getOptions,
+      };
+    }
 
     return (
       <div className={`record-autocomplete ${className}`}>
         <NoSsr>
-          <AsyncSelect
+          <BaseSelectComponent
             value={selected}
             cacheOptions={!disableCache}
-            defaultOptions={minSearchLength === 0}
             components={components}
             DetailChipProps={DetailChipProps}
             error={Boolean(errorText)}
@@ -207,7 +237,6 @@ class RecordAutocomplete extends React.Component {
             isClearable={!disabled}
             isMulti={isMulti}
             isSearchable={!disabled}
-            loadOptions={this.getOptions}
             placeholder={
               disabled
                 ? ''
@@ -226,6 +255,7 @@ class RecordAutocomplete extends React.Component {
               required,
               label,
             }}
+            {...uniqueProps}
           />
         </NoSsr>
       </div>

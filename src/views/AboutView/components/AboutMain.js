27db416e7986a9cb9bc2fbcdf241d7bbe8d00a84
Caralyn Reisle <creisle@bcgsc.ca>
2019-03-27 17:06:26 -0700

    Fix spacing after removing absolute parent

--- src/views/AboutView/components/AboutMain.js
+++ src/views/AboutView/components/AboutMain.js
@@ -64,7 +64,7 @@ class AboutMain extends Component {
     const call = api.get(`${model.routeName}?limit=1&neighbors=1`);
     this.controllers.push(call);
     const result = await call.request();
-    if (result.length) {
+    if (result && result.length) {
       this.setState({ [`${model.name}-example`]: result[0] });
     }
   }

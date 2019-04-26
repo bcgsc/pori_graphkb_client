5441b87455f8962dcc0ed3f7be2b292d63dc3695
Caralyn Reisle <creisle@bcgsc.ca>
2019-04-26 15:42:47 -0700

    KBDEV-499: Use multiple webpack configs

---
+++ config/webpack/dev.js
@@ -0,0 +1,39 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+
+const common = require('./prod.js');
+
+
+const devConfig = {
+  mode: 'development',
+  plugins: [
+    // Copy values of ENV variables in as strings using these defaults (null = unset)
+    new webpack.EnvironmentPlugin({
+      API_BASE_URL: 'http://kbapi01:8081/api',
+      DEBUG: false,
+      DISABLE_AUTH: null,
+      KEYCLOAK_CLIENT_ID: 'GraphKB',
+      KEYCLOAK_REALM: 'TestKB',
+      KEYCLOAK_ROLE: 'GraphKB',
+      KEYCLOAK_URL: 'http://ga4ghdev01.bcgsc.ca:8080/auth',
+      NODE_ENV: 'development',
+      USER: null,
+      PASSWORD: null,
+      npm_package_version: null,
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserWebpackPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          module: true,
+          sourceMap: true,
+        },
+      }),
+    ],
+  },
+};
+module.exports = merge(common, devConfig);

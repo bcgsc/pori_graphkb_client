3165fc4f434a840223fb61f14881cbcb10086977
Caralyn Reisle <creisle@bcgsc.ca>
2019-04-26 16:45:35 -0700

    KBDEV-499: Add dev/local modes

--- config/webpack/local.js
+++ config/webpack/local.js
@@ -7,18 +7,18 @@ const common = require('./common.js');
 
 
 const devConfig = {
-  mode: 'production',
+  mode: 'development',
   plugins: [
     // Copy values of ENV variables in as strings using these defaults (null = unset)
     new webpack.EnvironmentPlugin({
-      API_BASE_URL: 'https://graphkb-api.bcgsc.ca/api',
+      API_BASE_URL: `http://${process.env.HOSTNAME}:8080/api`,
       DEBUG: false,
       DISABLE_AUTH: null,
       KEYCLOAK_CLIENT_ID: 'GraphKB',
-      KEYCLOAK_REALM: 'GSC',
+      KEYCLOAK_REALM: 'TestKB',
       KEYCLOAK_ROLE: 'GraphKB',
-      KEYCLOAK_URL: 'https://sso.bcgsc.ca/auth',
-      NODE_ENV: 'production',
+      KEYCLOAK_URL: 'http://ga4ghdev01.bcgsc.ca:8080/auth',
+      NODE_ENV: 'local',
       USER: null,
       PASSWORD: null,
       npm_package_version: null,
@@ -30,6 +30,7 @@ const devConfig = {
         terserOptions: {
           keep_classnames: true,
           module: true,
+          sourceMap: true,
         },
       }),
     ],

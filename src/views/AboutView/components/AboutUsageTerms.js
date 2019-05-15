ad861e4b3d3dbec2742b02840a706ac36b5a10ad
David Pham <dpham@dpham01.phage.bcgsc.ca>
2019-05-15 08:30:35 -0700

    PR#137 Combine section and TOC creation into a single loop for performance gain

--- src/views/AboutView/components/AboutUsageTerms.js
+++ src/views/AboutView/components/AboutUsageTerms.js
@@ -16,32 +16,36 @@ const AboutUsageTerms = () => {
     { label: 'Limitation of Liability', id: 'limits', content: 'Aliquip culpa aute eiusmod aliquip enim irure in minim ut pariatur incididunt. Ad do aliquip quis sint sint sit aliquip sit esse ex. Incididunt id commodo exercitation sint magna officia aute. Labore ea proident do labore veniam dolore non ipsum. Tempor nostrud ut ullamco voluptate esse minim reprehenderit mollit aliquip id ut sint officia. Excepteur in commodo laborum sint incididunt laborum. Aliqua cillum excepteur exercitation consequat et aliquip.' },
   ];
 
-  const tableOfContents = sectionData.map((section) => {
-    const anchorId = section.id;
-    return (
+  const tableOfContents = [];
+  const sections = [];
+
+  sectionData.forEach((sectionDatum) => {
+    const anchorId = sectionDatum.id;
+    const tocSection = (
       <ListItem>
         <ListItemIcon className="letter-icon">
-          {section.label.slice(0, 1)}
+          {sectionDatum.label.slice(0, 1)}
         </ListItemIcon>
         <ListItemText>
-          <a href={`about/terms#${anchorId}`}> {section.label}</a>
+          <a href={`about/terms#${anchorId}`}> {sectionDatum.label}</a>
         </ListItemText>
       </ListItem>
     );
-  });
+    tableOfContents.push(tocSection);
 
-  const sections = sectionData.map(section => (
-    (
+    const section = (
       <div>
-        <Typography variant="h6" component="h3" id={section.id}>
-          {section.label}
+        <Typography variant="h6" component="h3" id={sectionDatum.id}>
+          {sectionDatum.label}
         </Typography>
         <Typography paragraph>
-          {section.content}
+          {sectionDatum.content}
         </Typography>
       </div>
-    )
-  ));
+    );
+    sections.push(section);
+  });
+
 
   return (
     <div className="about-page__content">

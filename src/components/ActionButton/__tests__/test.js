d67105319d03fa528c15823ef45162bc4e0d619c
David Pham <dpham@dpham01.phage.bcgsc.ca>
2019-05-28 08:15:58 -0700

    Update ActionButton test to reflect migration to react hooks
    Testing hook methods is not supported by enzyme. Since this component and
    it's hooks are tightly related to each other. Testing it's output and behaviour
    was done rather than testing the hooks directly.

--- src/components/ActionButton/__tests__/test.js
+++ src/components/ActionButton/__tests__/test.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { Button } from '@material-ui/core';
 
 import ActionButton from '..';
 import ConfirmActionDialog from '../ConfirmActionDialog';
 
 
 describe('ActionButton', () => {
-  // const dialogOpenSpy = jest.spyOn(ActionButton.prototype, 'handleOpenDialog');
   const onClick = jest.fn();
 
-  test('does not use dialog when no requestConfirm flag is given', async () => {
+  test('uses onClick when requireConfirm flag is false', async () => {
     const wrapper = mount((
       <ActionButton
         title="action"
@@ -28,7 +26,7 @@ describe('ActionButton', () => {
     expect(wrapper.find(ConfirmActionDialog).exists()).toEqual(false);
   });
 
-  test('uses dialog when requestConfirm flag is given', async () => {
+  test('does not use onClick when requireConfirm flag is given', async () => {
     const wrapper = mount((
       <ActionButton
         title="action"
@@ -38,45 +36,13 @@ describe('ActionButton', () => {
         action
       </ActionButton>
     ));
-    wrapper.update();
-    expect(wrapper.find(ConfirmActionDialog).exists()).toEqual(true);
-    expect(wrapper.find(Button)).toHaveLength(1);
-    const baseBtn = wrapper.find(Button).instance();
-    expect(baseBtn.props.className).toEqual('action-button__button');
-    const testSpy = jest.fn();
-    console.log('baseBtn prop : ', baseBtn.props);
-    expect(baseBtn.props.onClick).not.toEqual(onClick);
-    // set Button component up with new props.
-    await wrapper.setProps({
-      children: (
-        <Button
-          variant={null}
-          onClick={
-            testSpy
-          }
-          size="large"
-          color="primarytest"
-          className="action-button__button"
-          disabled={false}
-        >
-          {'action'}
-        </Button>
-      ),
-    });
-
-    console.log(wrapper.find(Button).at(1).instance().props);
-    expect(wrapper.find(Button).at(1).instance().props.onClick).toEqual(testSpy);
-    await wrapper.find(Button).at(1).prop('onClick')();
+    await wrapper.find('button').at(0).prop('onClick')();
     wrapper.update();
     // check that the onClick handler was not called
     expect(onClick).not.toHaveBeenCalled();
-    // check that the dialog handler was called
-    expect(testSpy).toHaveBeenCalled();
-    console.log('testSpy : ', testSpy.mock);
-  });
-
+    expect(wrapper.find(ConfirmActionDialog).exists()).toEqual(true);
   // TODO: currently cannot test confirm or cancel clicks since the dialog contents renders outside the wrapper element
-
+  });
   afterEach(() => {
     jest.resetAllMocks();
   });

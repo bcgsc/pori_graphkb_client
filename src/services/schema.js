27745e75c8f618d0e060947309568b41e0650404
David Pham <dpa35@sfu.ca>
2019-08-15 14:50:59 -0700

    Update Schema Package to latest version

--- src/services/schema.js
+++ src/services/schema.js
@@ -115,6 +115,10 @@ class Schema {
       if (obj['@rid']) {
         return obj['@rid'];
       }
+      console.log('schema -> obj', obj);
+      if (Array.isArray(obj)) { // embedded link set
+        return obj.length;
+      }
     }
     return obj;
   }

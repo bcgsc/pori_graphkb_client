088e5d99e641f9b7d535ce66eaa4464802a439da
David Pham <dpa35@sfu.ca>
2019-09-16 08:45:41 -0700

    PR#171 Move static method out of DataView class
    Moved hashRecordsByRID into util module to seperate from Dataview
    class and to increase usuability.

---
+++ src/views/DataView/util.js
@@ -0,0 +1,18 @@
+
+const isObject = (val) => {
+  if (val === null) { return false; }
+  return ((typeof val === 'function') || (typeof val === 'object'));
+};
+
+const hashRecordsByRID = (data) => {
+  const newData = {};
+  data.forEach((obj) => {
+    newData[obj['@rid']] = obj;
+  });
+  return newData;
+}
+
+export {
+  isObject,
+  hashRecordsByRID,
+};

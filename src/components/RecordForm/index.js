e6c1f96430fba2eea1aacea168eba8534f297c60
Caralyn Reisle <creisle@bcgsc.ca>
2019-09-20 13:25:44 -0700

    Refactor RecordForm/index.js
    - Convert to hooks
    - Lift up form state to live in this top-level component
    - Only handle edit&delete/new/view (no search)
    - Use base record form for layout only
    
    Addresses: KBDEV-613

--- src/components/RecordForm/index.js
+++ src/components/RecordForm/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, {
+  useEffect, useContext, useState, useReducer, useCallback,
+} from 'react';
+import useDeepCompareEffect from 'use-deep-compare-effect';
 import PropTypes from 'prop-types';
 import {
-  Paper, Typography, Button,
+  Paper, Typography, Button, CircularProgress,
 } from '@material-ui/core';
-import { boundMethod } from 'autobind-decorator';
 import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
 
 import { SnackbarContext } from '@bcgsc/react-snackbar-provider';
 
 import api from '../../services/api';
+
 import './index.scss';
+import ActionButton from '../ActionButton';
 import FormLayout from './FormLayout';
-import { FORM_VARIANT } from './util';
-import { withKB } from '../KBContext';
+import {
+  FORM_VARIANT,
+
+} from './util';
+import { KBContext } from '../KBContext';
 import ReviewDialog from './ReviewDialog';
 import ToggleButtonGroup from '../ToggleButtonGroup';
 
@@ -23,7 +30,7 @@ const cleanPayload = (payload) => {
   }
   const newPayload = {};
   Object.entries(payload).forEach(([key, value]) => {
-    if (value !== undefined) {
+    if (value !== undefined && !/^(in|out)_\w+$/.exec(key)) {
       if (typeof value === 'object' && value !== null) {
         if (Array.isArray(value)) {
           newPayload[key] = value.map((arr) => {
@@ -56,140 +63,78 @@ const cleanPayload = (payload) => {
  * @property {object} props.schema schema object defining class/prop models
  * @property {value} props.value values of individual properties of passed class model
  */
-class RecordForm extends React.PureComponent {
-  static contextType = SnackbarContext;
+const RecordForm = ({
+  value: initialValue,
+  modelName,
+  title,
+  onTopClick,
+  onSubmit,
+  onError,
+  variant,
+  ...rest
+}) => {
+  const snackbar = useContext(SnackbarContext);
+  const { schema } = useContext(KBContext);
 
-  static propTypes = {
-    modelName: PropTypes.string,
-    onError: PropTypes.func,
-    onSubmit: PropTypes.func,
-    onTopClick: PropTypes.func,
-    rid: PropTypes.string,
-    schema: PropTypes.object.isRequired,
-    title: PropTypes.string.isRequired,
-    variant: PropTypes.string,
-    value: PropTypes.object,
-  };
+  const [actionInProgress, setActionInProgress] = useState(false);
+  const controllers = [];
+  const [reviewDialogOpen, setReviewDialogOpen] = useState(false);
 
-  static defaultProps = {
-    modelName: null,
-    onError: () => {},
-    onSubmit: () => {},
-    onTopClick: null,
-    rid: null,
-    variant: FORM_VARIANT.VIEW,
-    value: {},
-  };
+  // handle and store the form content
+  const [formContent, setFormFieldContent] = useReducer((state, action) => {
+    const { type: actionType, payload } = action;
 
-  constructor(props) {
-    super(props);
-    const { value = {}, modelName } = this.props;
-
-    const defaultContent = { ...value };
-
-    if (modelName && !defaultContent['@class']) {
-      defaultContent['@class'] = modelName;
+    if (actionType === 'update') {
+      const { name, value } = payload;
+      return { ...state, [name]: value };
+    } if (actionType === 'replace') {
+      return { ...payload };
     }
+    throw new Error(`actionType (${actionType}) not implemented`);
+  }, initialValue);
 
-    this.state = {
-      actionInProgress: false,
-      reviewDialogOpen: false,
-      newReview: {},
-      ...defaultContent,
-    };
-    this.controllers = [];
-  }
+  // handle and store any errors reported from form field validators
+  const [formErrors, setFormFieldError] = useReducer((state, action) => {
+    const { type: actionType, payload } = action;
 
-  async componentDidMount() {
-    await this.getNodeFromUri();
-  }
-
-  async componentDidUpdate(prevProps) {
-    const {
-      rid,
-      variant,
-      modelName,
-      value: { '@class': defaultModel },
-    } = this.props;
-
-    if (
-      rid !== prevProps.rid
-      || variant !== prevProps.variant
-      || modelName !== prevProps.modelName
-      || defaultModel !== prevProps.value['@class']
-    ) {
-      await this.getNodeFromUri();
+    if (actionType === 'update') {
+      const { name, value } = payload;
+      return { ...state, [name]: value };
+    } if (actionType === 'replace') {
+      return { ...payload };
     }
-  }
+    throw new Error(`actionType (${actionType}) not implemented`);
+  }, {});
 
-  componentWillUnmount() {
-    this.controllers.map(c => c.abort());
-    this.controllers = [];
-  }
+  const formHasErrors = Object.values(formErrors).some(err => err);
 
-  /**
-   * Issues a GET request to the API to retrieve the record contents
-   */
-  async getNodeFromUri() {
-    // parse the node ident from the uri
-    const {
-      rid,
-      variant,
-      onError,
-      schema,
-      modelName,
-      value,
-    } = this.props;
+  useEffect(() => () => controllers.map(c => c.abort()), []); // eslint-disable-line
 
-    const { '@class': defaultModel } = value;
-
-    const model = schema.get(modelName || defaultModel || modelName || 'V');
-
-    if (variant !== FORM_VARIANT.NEW && variant !== FORM_VARIANT.SEARCH) {
-      // If not a new form then should have existing content
-      this.setState({ actionInProgress: true });
-
-      try {
-        const call = api.get(`${model.routeName}/${rid.replace(/^#/, '')}?neighbors=3`, { forceListReturn: true });
-        this.controllers.push(call);
-        const result = await call.request();
-
-        if (result && result.length) {
-          this.setState({ ...result[0] });
-        } else {
-          onError({ error: { name: 'RecordNotFound', message: `Unable to retrieve record details for ${rid}` } });
-        }
-      } catch (err) {
-        onError({ error: err });
-      }
-      this.setState({ actionInProgress: false });
-    }
-  }
+  useDeepCompareEffect(() => {
+    setFormFieldContent({ type: 'replace', payload: initialValue });
+    setFormFieldError({ type: 'replace', payload: {} });
+  }, [initialValue]);
 
   /**
    * Handler for submission of a new record
    */
-  @boundMethod
-  async handleNewAction({ content, errors }) {
-    const snackbar = this.context;
-    const {
-      schema, onSubmit, onError, modelName,
-    } = this.props;
-
-    if (errors && Object.keys(errors).length) {
+  const handleNewAction = useCallback(async () => {
+    if (formHasErrors) {
       // bring up the snackbar for errors
-      console.error(errors);
+      console.error(formErrors);
       snackbar.add('There are errors in the form which must be resolved before it can be submitted');
     } else {
       // ok to POST
-      if (!content || !content['@class']) {
+      const content = { ...formContent };
+
+      if (!formContent['@class']) {
         content['@class'] = modelName;
       }
       const payload = cleanPayload(content);
       const { routeName } = schema.get(payload);
       const call = api.post(routeName, payload);
-      this.controllers.push(call);
-      this.setState({ actionInProgress: true });
+      controllers.push(call);
+      setActionInProgress(true);
 
       try {
         const result = await call.request();
@@ -200,22 +145,23 @@ class RecordForm extends React.PureComponent {
         snackbar.add(`Error (${err.name}) in creating the record`);
         onError({ error: err, content });
       }
-      this.setState({ actionInProgress: false });
+      setActionInProgress(false);
     }
-  }
+  }, [controllers, formContent, formErrors, formHasErrors, modelName, onError, onSubmit, schema, snackbar]);
 
   /**
    * Handler for deleting an existing record
    */
-  @boundMethod
-  async handleDeleteAction({ content }) {
-    const snackbar = this.context;
-    const { schema, onSubmit, onError } = this.props;
+  const handleDeleteAction = useCallback(async () => {
+    const content = { ...formContent };
 
+    if (!formContent['@class']) {
+      content['@class'] = modelName;
+    }
     const { routeName } = schema.get(content);
     const call = api.delete(`${routeName}/${content['@rid'].replace(/^#/, '')}`);
-    this.controllers.push(call);
-    this.setState({ actionInProgress: true });
+    controllers.push(call);
+    setActionInProgress(true);
 
     try {
       await call.request();
@@ -225,28 +171,30 @@ class RecordForm extends React.PureComponent {
       snackbar.add(`Error (${err.name}) in deleting the record (${content['@rid']})`);
       onError({ error: err, content });
     }
-    this.setState({ actionInProgress: false });
-  }
+    setActionInProgress(false);
+  }, [controllers, formContent, modelName, onError, onSubmit, schema, snackbar]);
 
   /**
    * Handler for edits to an existing record
    */
-  @boundMethod
-  async handleEditAction({ content: { impliedBy, supportedBy, ...content }, errors }) {
-    const snackbar = this.context;
-    const { schema, onSubmit, onError } = this.props;
+  const handleEditAction = useCallback(async () => {
+    const content = { ...formContent };
 
-    if (errors && Object.keys(errors).length) {
+    if (!formContent['@class']) {
+      content['@class'] = modelName;
+    }
+
+    if (formHasErrors) {
       // bring up the snackbar for errors
-      console.error(errors);
+      console.error(formErrors);
       snackbar.add('There are errors in the form which must be resolved before it can be submitted');
     } else {
       // ok to PATCH
       const payload = cleanPayload(content);
       const { routeName } = schema.get(payload);
       const call = api.patch(`${routeName}/${content['@rid'].replace(/^#/, '')}`, payload);
-      this.controllers.push(call);
-      this.setState({ actionInProgress: true });
+      controllers.push(call);
+      setActionInProgress(true);
 
       try {
         const result = await call.request();
@@ -256,115 +204,145 @@ class RecordForm extends React.PureComponent {
         snackbar.add(`Error (${err.name}) in editing the record (${content['@rid']})`);
         onError({ error: err, content });
       }
-      this.setState({ actionInProgress: false });
+      setActionInProgress(false);
     }
-  }
+  }, [controllers, formContent, formErrors, formHasErrors, modelName, onError, onSubmit, schema, snackbar]);
 
-  @boundMethod
-  async handleSearchAction({ content, errors }) {
-    const snackbar = this.context;
-    const { onSubmit } = this.props;
+  const handleOnChange = (event) => {
+    // add the new value to the field
+    const eventName = event.target.name || event.target.getAttribute('name'); // name of the form field triggering the event
+    const eventValue = event.target.value;
 
-    if (errors && Object.keys(errors).length) {
-      console.error(errors);
-      snackbar.add('There are errors in the form which must be resolved before it can be submitted');
-    } else {
-      onSubmit(content);
-      snackbar.add('Search Submitted. Redirecting to the data view');
+    const { properties: { [eventName]: prop } } = schema.get(modelName);
+    const { value, error } = schema.validateValue(prop, eventValue, false);
+
+    setFormFieldContent({ type: 'update', payload: { name: eventName, value } });
+    setFormFieldError({ type: 'update', payload: { name: eventName, value: error } });
+  };
+
+  const handleAddReview = useCallback((content, updateReviewStatus) => {
+    // add the new value to the field
+    const reviews = [...(formContent.reviews || []), content];
+    setFormFieldContent({ type: 'update', payload: { name: 'reviews', value: reviews } });
+
+    if (updateReviewStatus) {
+      setFormFieldContent({ type: 'update', payload: { name: 'reviewStatus', value: content.status } });
     }
-  }
+    setReviewDialogOpen(false);
+  }, [formContent]);
 
-  @boundMethod
-  handleNewReviewUpdate(updatedReview) {
-    this.setState({ newReview: updatedReview });
-  }
+  const isEdge = false; // TODO
 
-  @boundMethod
-  handleContentUpdate(content) {
-    this.setState({ ...content });
-  }
-
-  render() {
-    const {
-      title, variant, onTopClick, modelName, onError, ...rest
-    } = this.props;
-
-
-    const {
-      actionInProgress,
-      reviewDialogOpen,
-      newReview,
-      ...content
-    } = this.state;
-
-    const snackbar = this.context;
-
-    const actions = {
-      [FORM_VARIANT.EDIT]: this.handleEditAction,
-      [FORM_VARIANT.NEW]: this.handleNewAction,
-      [FORM_VARIANT.SEARCH]: this.handleSearchAction,
-    };
-
-    return (
-      <Paper className="record-form__wrapper" elevation={4}>
-        <div className="record-form__header">
-          <Typography variant="h1" className="title">{title}</Typography>
-          <div className="header-action-buttons">
-            {(content['@class'] === 'Statement' && variant === FORM_VARIANT.EDIT && (
-            <Button
-              onClick={() => this.setState({ reviewDialogOpen: true })}
-              variant="outlined"
-              disabled={actionInProgress}
-            >
-              <LocalLibraryIcon
-                classes={{ root: 'review-icon' }}
-              />
-                Add Review
-            </Button>
-            ))}
-            {onTopClick && (variant === FORM_VARIANT.VIEW || variant === FORM_VARIANT.EDIT) && (
-              <ToggleButtonGroup
-                onClick={() => onTopClick(content)}
-                requireConfirm
-                options={['view', 'edit']}
-                variant={variant}
-                message="Are you sure? You will lose your changes."
-              />
-            )}
-          </div>
-          {(content['@class'] === 'Statement' && variant === FORM_VARIANT.EDIT && (
-            <ReviewDialog
-              isOpen={reviewDialogOpen}
-              onClose={() => this.setState({ reviewDialogOpen: false })}
-              content={content}
-              newReview={newReview}
-              updateNewReview={this.handleNewReviewUpdate}
-              updateContent={this.handleContentUpdate}
-              snackbar={snackbar}
-              onError={onError}
+  return (
+    <Paper className="record-form__wrapper" elevation={4}>
+      <div className="record-form__header">
+        <Typography variant="h1" className="title">{title}</Typography>
+        <div className="header-action-buttons">
+          {(modelName === 'Statement' && variant === FORM_VARIANT.EDIT && (
+          <Button
+            onClick={() => setReviewDialogOpen(true)}
+            variant="outlined"
+            disabled={actionInProgress}
+          >
+            <LocalLibraryIcon
+              classes={{ root: 'review-icon' }}
             />
+            Add Review
+          </Button>
           ))}
+          {onTopClick && (variant === FORM_VARIANT.VIEW || variant === FORM_VARIANT.EDIT) && (
+          <ToggleButtonGroup
+            onClick={() => onTopClick(formContent)}
+            requireConfirm
+            options={['view', 'edit']}
+            variant={variant}
+            message="Are you sure? You will lose your changes."
+          />
+          )}
         </div>
-        <FormLayout
-          {...rest}
-          value={content}
-          modelName={modelName}
-          onSubmit={actions[variant] || null}
-          onDelete={this.handleDeleteAction}
-          reviewProps={{
-            updateContent: this.handleContentUpdate,
-            content,
-          }}
-          variant={variant}
-          collapseExtra
-          name="name"
-          actionInProgress={actionInProgress}
+        {(modelName === 'Statement' && variant === FORM_VARIANT.EDIT && (
+        <ReviewDialog
+          isOpen={reviewDialogOpen}
+          onClose={() => setReviewDialogOpen(false)}
+          onSubmit={handleAddReview}
         />
-      </Paper>
-    );
-  }
-}
+        ))}
+      </div>
+      <FormLayout
+        {...rest}
+        content={formContent}
+        errors={formErrors}
+        onChange={handleOnChange}
+        modelName={modelName}
+        variant={variant}
+        collapseExtra
+        disabled={actionInProgress || variant === FORM_VARIANT.VIEW}
+      />
+      {(
+        <div className="record-form__action-buttons">
+          {variant === FORM_VARIANT.EDIT
+            ? (
+              <ActionButton
+                onClick={() => handleDeleteAction}
+                variant="outlined"
+                size="large"
+                message="Are you sure you want to delete this record?"
+                disabled={actionInProgress}
+              >
+                DELETE
+              </ActionButton>
+            )
+            : (<div />) // for spacing issues only
+          }
+          {actionInProgress && (
+            <CircularProgress size={50} />
+          )}
+          {variant === FORM_VARIANT.NEW || (variant === FORM_VARIANT.EDIT && !isEdge)
+            ? (
+              <ActionButton
+                onClick={variant === FORM_VARIANT.EDIT
+                  ? handleEditAction
+                  : handleNewAction
+                }
+                variant="contained"
+                color="primary"
+                size="large"
+                requireConfirm={false}
+                disabled={actionInProgress || formHasErrors}
+              >
+                {variant === FORM_VARIANT.EDIT
+                  ? 'SUBMIT CHANGES'
+                  : 'SUBMIT'
+                }
+              </ActionButton>
+            )
+            : (<div />) // for spacing issues only
+          }
+        </div>
+      )}
+    </Paper>
+  );
+};
 
-export { RecordForm as RawRecordForm };
+RecordForm.propTypes = {
+  modelName: PropTypes.string,
+  onError: PropTypes.func,
+  onSubmit: PropTypes.func,
+  onTopClick: PropTypes.func,
+  rid: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+  value: PropTypes.object,
+};
 
-export default withKB(RecordForm);
+RecordForm.defaultProps = {
+  modelName: null,
+  onError: () => {},
+  onSubmit: () => {},
+  onTopClick: null,
+  rid: null,
+  variant: FORM_VARIANT.VIEW,
+  value: {},
+};
+
+export default RecordForm;

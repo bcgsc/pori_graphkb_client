f9b935c4382841726abdfde40138bcf9b924f50a
David Pham <dpa35@sfu.ca>
2019-10-09 15:56:30 -0700

    Add operator check to attr/value pairs and descriptions

--- src/views/AdvancedSearchView/constants.js
+++ src/views/AdvancedSearchView/constants.js
@@ -1,5 +1,29 @@
-const OPERATORS = ['=', 'CONTAINS', 'CONTAINSALL', 'CONTAINSTEXT', 'CONTAINSANY', 'IN', '>=', '>', '<=', '<', 'IS'];
+const OPERATORS_OPTS = ['=', 'CONTAINS', 'CONTAINSALL', 'CONTAINSTEXT', 'CONTAINSANY', 'IN', '>=', '>', '<=', '<', 'IS'];
 
+const generateOperatorOptions = () => {
+  const OPERATORS = [];
+  OPERATORS_OPTS.forEach((op) => {
+    const operatorOpt = {};
+    ['label', 'value', 'key'].forEach((val) => { operatorOpt[val] = op; });
+    OPERATORS.push(operatorOpt);
+  });
+
+  const findOptIndex = (operatorName) => {
+    const index = OPERATORS.findIndex(opt => opt.label === operatorName);
+    return index;
+  };
+
+  const iterableCaption = 'To be used on iterables such as appliedTo, impliedBy etc.';
+
+  OPERATORS[findOptIndex('CONTAINSALL')].caption = iterableCaption;
+  OPERATORS[findOptIndex('CONTAINSANY')].caption = iterableCaption;
+  OPERATORS[findOptIndex('CONTAINS')].caption = iterableCaption;
+  OPERATORS[findOptIndex('CONTAINSTEXT')].caption = 'contains your inputted substring';
+
+  return OPERATORS;
+};
+
+const OPERATORS = generateOperatorOptions();
 
 export {
   OPERATORS,
--- src/views/DataView/index.js
+++ src/views/DataView/index.js
@@ -80,7 +80,6 @@ class DataView extends React.Component {
       onErrorCallback: this.handleError,
     });
     const filters = await this.parseFilters(cache);
-    console.log('TCL: DataView -> componentDidMount -> filters', filters);
     this.setState({ cache, filters });
   }
 
@@ -97,19 +96,16 @@ class DataView extends React.Component {
    */
   async parseFilters(cache) {
     const { search } = this.state;
-    console.log('TCL: DataView -> parseFilters -> search', search);
     const { schema } = this.context;
 
     try {
       const { queryParams, modelName } = api.getQueryFromSearch({ search, schema });
-      console.log('TCL: DataView -> parseFilters -> queryParams, modelName', queryParams, modelName);
       const links = [];
       Object.entries(queryParams || {}).forEach(([key, value]) => {
         if (typeof value === 'string' && kbSchema.util.looksLikeRID(value)) {
           links.push({ key, value });
         }
       });
-      console.log('TCL: DataView -> parseFilters -> links', links);
 
 
       const records = await cache.getRecords(links.map(l => ({ '@rid': l.value })));

32d35af4f7207c0fa7a7b7e4964c1afbfc64193a
David Pham <dpa35@sfu.ca>
2019-10-10 13:27:32 -0700

    Collapse popular search tabs and update api default suggestion handler
    use base popular search component and pass in search info. New api
    query endpoint used for default suggestion handler

--- src/views/PopularSearchView/components/DiseaseSearch.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DiseaseSearch() {
-  return (
-    <h1>DiseaseSearch</h1>
-  );
-}
-
-export default DiseaseSearch;
--- src/views/PopularSearchView/components/util.js
+++ src/views/PopularSearchView/components/util.js
@@ -2,68 +2,68 @@ const SEARCH_OPTS = {
   DRUG: [
     {
       label: 'Given a drug, find all variants associated with therapeutic sensitivity',
-      requiredInput: { class: 'Drug', property: 'name' },
+      requiredInput: { label: 'Drug', property: 'name', class: 'Therapy' },
     },
     {
       label: 'Given a drug, find all varaints associated with resistance',
-      requiredInput: { class: 'Drug', property: 'name' },
+      requiredInput: { label: 'Drug', property: 'name', class: 'Therapy' },
     },
     {
       label: 'Given a drug, find all variants with pharmacogenomic information',
-      requiredInput: { class: 'Drug', property: 'name' },
+      requiredInput: { label: 'Drug', property: 'name', class: 'Therapy' },
     },
     {
       label: 'Given a drug, find all disease it is approved for use',
-      requiredInput: { class: 'Drug', property: 'name' },
+      requiredInput: { label: 'Drug', property: 'name', class: 'Therapy' },
     },
   ],
   DISEASE: [
     {
       label: 'Given a disease, find all genes associaed with therapeutic sensitivity',
-      requiredInput: { class: 'Disease', property: 'name' },
+      requiredInput: { label: 'Disease', property: 'name', class: 'Disease' },
     },
     {
       label: 'Given a disease, find all genes associated with therapeutic resistance',
-      requiredInput: { class: 'Disease', property: 'name' },
+      requiredInput: { label: 'Disease', property: 'name', class: 'Disease' },
     },
     {
       label: 'Given a disease, find all variants associated with a relevance',
-      requiredInput: { class: 'Disease', property: 'name' },
+      requiredInput: { label: 'Disease', property: 'name', class: 'Disease' },
     },
   ],
   VARIANT: [
     {
       label: 'Given a variant, find all therapies associaed with sensitivity for Disease(s)',
-      requiredInput: { class: 'Variant', property: 'name' },
-      optionalInput: { class: 'Disease', property: 'name' },
+      requiredInput: { label: 'Variant', property: 'name', class: 'Variant' },
+      optionalInput: { label: 'Disease', property: 'name', class: 'Disease' },
     },
     {
       label: 'Given a variant, find all therapies associated with resistance',
-      requiredInput: { class: 'Variant', property: 'name' },
-      optionalInput: { class: 'Disease', property: 'name' },
+      requiredInput: { label: 'Variant', property: 'name', class: 'Variant' },
+      optionalInput: { label: 'Disease', property: 'name', class: 'Disease' },
     },
     {
       label: 'Given a variant, find all disease that the variant is associated with',
-      requiredInput: { class: 'Variant', property: 'name' },
-      optionalInput: { class: 'Relevance', property: 'name' },
+      requiredInput: { label: 'Variant', property: 'name', class: 'Variant' },
+      optionalInput: { label: 'Relevance', property: 'name', class: 'Vocabulary' },
     },
   ],
   GENE: [
     {
       label: 'Given a gene, find all variants reported for all diseases',
-      requiredInput: { class: 'Gene', property: 'name' },
+      requiredInput: { label: 'Gene', property: 'name', class: 'Feature' },
     },
     {
       label: 'Given a gene, find a specific disease and the associated relevances',
-      requiredInput: { class: 'Gene', property: 'name' },
+      requiredInput: { label: 'Gene', property: 'name', class: 'Feature' },
     },
     {
       label: 'Given a gene, find all variants linked with drug sensitivity or resistance',
-      requiredInput: { class: 'Gene', property: 'name' },
+      requiredInput: { label: 'Gene', property: 'name', class: 'Feature' },
     },
     {
       label: 'Given a gene, find all variants on the ',
-      requiredInput: { class: 'Gene', property: 'name' },
+      requiredInput: { label: 'Gene', property: 'name', class: 'Feature' },
     },
   ],
 };

deb938c35dcfab97d350a8ca01644943baeef211
David Pham <dpa35@sfu.ca>
2019-10-21 14:04:14 -0700

    PR#198 Resolved first round of PR reviewers tasks
    remove outdated QueryBuilderView and related files.
    Add a description to AdvancedSearchView component.
    Add docstring to Operators options to describe shape.
    update api tests to reflect new /query endpoint change.
    In statmentSentence filter out subject from condition if duplicated
    Re-add orderBy option to default suggestion handler

--- src/services/api/__tests__/index.js
+++ src/services/api/__tests__/index.js
@@ -6,83 +6,107 @@ describe('api methods test', () => {
   describe('getSearchFromQuery', () => {
     const schema = { getFromRoute: () => ({ name: 'disease' }) };
 
-    test('keyword search', () => {
-      const queryParams = {
-        keyword: 'kras',
-        limit: 50,
-        neighbors: 4,
-      };
-      const result = api.getSearchFromQuery({
-        schema, routeName: '/search', queryParams,
-      });
-      expect(result).toEqual(
-        'keyword=kras&limit=50&neighbors=4',
-      );
-    });
-
-    test('complex search', () => {
+    test('complex keyword search', () => {
       const payload = {
-        where: [
-          { operator: 'OR', comparisons: [{ attr: 'name', value: 'kras', operator: 'CONTAINSTEXT' }] },
-        ],
-        limit: 50,
+        target: 'Disease',
+        queryType: 'keyword',
+        keyword: 'kras',
       };
       const result = api.getSearchFromQuery({
-        schema, routeName: '/diseases/search', payload,
+        schema, routeName: '/query', payload,
       });
-      const search = `class=disease&complex=${
-        encodeURIComponent('eyJ3aGVyZSI6W3sib3BlcmF0b3IiOiJPUiIsImNvbXBhcmlzb25zIjpbeyJhdHRyIjoibmFtZSIsInZhbHVlIjoia3JhcyIsIm9wZXJhdG9yIjoiQ09OVEFJTlNURVhUIn1dfV0sImxpbWl0Ijo1MH0=')
-      }`;
+      const search = 'class=disease&complex=eyJ0YXJnZXQiOiJEaXNlYXNlIiwicXVlcnlUeXBlIjoia2V5d29yZCIsImtleXdvcmQiOiJrcmFzIn0%3D';
       expect(result).toEqual(search);
     });
 
-    test('general search', () => {
-      const queryParams = {
-        name: 'kras',
-        sourceId: 'kras',
-        or: 'name,sourceId',
+    test('keyword search with additional params', () => {
+      const payload = {
+        keyword: 'kras',
+        target: 'Disease',
+        queryType: 'keyword',
         limit: 50,
         neighbors: 4,
       };
       const result = api.getSearchFromQuery({
-        schema, routeName: '/diseases', queryParams,
+        schema, routeName: '/query', payload,
       });
       expect(result).toEqual(
-        `class=disease&limit=50&name=kras&neighbors=4&or=name${
-          encodeURIComponent(',')
-        }sourceId&sourceId=kras`,
+        'class=disease&complex=eyJrZXl3b3JkIjoia3JhcyIsInRhcmdldCI6IkRpc2Vhc2UiLCJxdWVyeVR5cGUiOiJrZXl3b3JkIiwibGltaXQiOjUwLCJuZWlnaGJvcnMiOjR9',
+      );
+    });
+
+    test('complex query', () => {
+      const payload = {
+        target: 'Statement',
+        filters: {
+          OR: [
+            {
+              conditions: {
+                queryType: 'keyword',
+                keyword: 'cancer',
+                target: 'Disease',
+              },
+            },
+            {
+              conditions: {
+                queryType: 'keyword',
+                keyword: 'colon',
+                target: 'Disease',
+              },
+            },
+          ],
+        },
+      };
+      const result = api.getSearchFromQuery({
+        schema, routeName: '/query', payload,
+      });
+      expect(result).toEqual(
+        'class=disease&complex=eyJ0YXJnZXQiOiJTdGF0ZW1lbnQiLCJmaWx0ZXJzIjp7Ik9SIjpbeyJjb25kaXRpb25zIjp7InF1ZXJ5VHlwZSI6ImtleXdvcmQiLCJrZXl3b3JkIjoiY2FuY2VyIiwidGFyZ2V0IjoiRGlzZWFzZSJ9fSx7ImNvbmRpdGlvbnMiOnsicXVlcnlUeXBlIjoia2V5d29yZCIsImtleXdvcmQiOiJjb2xvbiIsInRhcmdldCI6IkRpc2Vhc2UifX1dfX0%3D',
       );
     });
   });
 
   describe('getQueryFromSearch', () => {
-    const schema = { get: () => ({ routeName: '/diseases' }) };
+    const schema = {
+      get: () => ({ routeName: '/diseases' }),
+      getFromRoute: () => ({ name: 'disease' }),
+    };
 
     test('keyword search', () => {
-      const search = 'keyword=kras';
+      const search = 'keyword=kras&class=Disease';
       const result = api.getQueryFromSearch({
         search,
         schema,
       });
-      expect(result.payload).toBe(null);
+      const expectedPayload = {
+        keyword: 'kras',
+        queryType: 'keyword',
+        target: 'Disease',
+      };
+      expect(result.payload).toMatchObject(expectedPayload);
       expect(result.routeName).toEqual('/query');
-      expect(result.queryParams.keyword).toEqual('kras');
+      expect(result.payload.keyword).toEqual('kras');
     });
 
     test('complex search', () => {
-      const complex = {
-        where: [
-          { operator: 'OR', comparisons: [{ attr: 'name', value: 'kras', operator: 'CONTAINSTEXT' }] },
-        ],
+      const payload = {
+        target: 'Disease',
+        filters: {
+          OR: [
+            {
+              name: 'kras',
+              operator: 'CONTAINSTEXT',
+            },
+          ],
+        },
         limit: 50,
-        neighbors: 2, // adds default neighbors
+        neighbors: 2,
       };
-      const search = `class=disease&complex=${
-        encodeURIComponent('eyJ3aGVyZSI6W3sib3BlcmF0b3IiOiJPUiIsImNvbXBhcmlzb25zIjpbeyJhdHRyIjoibmFtZSIsInZhbHVlIjoia3JhcyIsIm9wZXJhdG9yIjoiQ09OVEFJTlNURVhUIn1dfV0sImxpbWl0Ijo1MH0=')
-      }`;
+
+      const search = 'class=disease&complex=eyJ0YXJnZXQiOiJEaXNlYXNlIiwiZmlsdGVycyI6eyJPUiI6W3sibmFtZSI6ImtyYXMiLCJvcGVyYXRvciI6IkNPTlRBSU5TVEVYVCJ9XX0sImxpbWl0Ijo1MCwibmVpZ2hib3JzIjoyfQ%3D%3D';
       const actualSearch = api.getQueryFromSearch({ schema, search });
       expect(actualSearch).toEqual({
-        payload: complex,
+        payload,
         routeName: '/query',
         queryParams: null,
         modelName: 'disease',
@@ -90,20 +114,32 @@ describe('api methods test', () => {
     });
 
     test('general search', () => {
-      const search = 'name=kras&sourceId=kras&or=name,sourceId';
+      const search = 'class=disease&complex=eyJxdWVyeVR5cGUiOiJzaW1pbGlhclRvIiwidGFyZ2V0IjoiRmVhdHVyZSIsImZpbHRlcnMiOnsiQU5EIjpbeyJiaW90eXBlIjoiZ2VuZSJ9LHsiT1IiOlt7Im5hbWUiOiJrcmFzIn0seyJzb3VyY2VJZCI6ImtyYXMifV19XX19';
       const result = api.getQueryFromSearch({
         search,
         schema,
       });
-      expect(result.payload).toBe(null);
-      expect(result.routeName).toEqual('/query');
-      expect(result.queryParams).toEqual({
-        name: 'kras',
-        sourceId: 'kras',
-        or: 'name,sourceId',
+
+      const payload = {
+        queryType: 'similiarTo',
+        target: 'Feature',
+        filters: {
+          AND: [
+            {
+              biotype: 'gene',
+            },
+            {
+              OR: [{ name: 'kras' }, { sourceId: 'kras' }],
+            },
+          ],
+        },
         limit: 100,
         neighbors: 2,
-      });
+      };
+
+      expect(result.payload).toMatchObject(payload);
+      expect(result.routeName).toEqual('/query');
+      expect(result.queryParams).toEqual(null);
     });
   });
 });

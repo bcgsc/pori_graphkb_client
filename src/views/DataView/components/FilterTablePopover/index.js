f24b949ce90993c06de639e71272835346d47e72
David Pham <dpham@dpham01.phage.bcgsc.ca>
2019-11-14 14:02:16 -0800

    Add filter group table popover to advanced search results
    add popover card to dataview. This includes toggle handlers and
    content managers.

--- src/views/DataView/components/FilterTablePopover/index.js
+++ src/views/DataView/components/FilterTablePopover/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Popover,
+  Avatar, Card, Chip, CardContent, Popover, Typography,
 } from '@material-ui/core';
+
+import './index.scss';
 
 function FilterTablePopover(props) {
   const {
     handleToggle, isOpen, anchorEl, filterGroups,
   } = props;
   console.log('TCL: FilterTablePopover -> filterGroups', filterGroups);
+
+  const renderRow = (filters) => {
+    console.log('TCL: FilterTablePopover -> filters', filters);
+    return (
+      <div className="filter-row">
+        <div className="filter-row__icon">
+          <Avatar
+            aria-label="Filter Group Icon"
+            className="filter-row__avatar"
+          >
+            FG
+          </Avatar>
+        </div>
+        <div className="filter-row__chips">
+          <>
+            {filters.map(filter => (
+              <div className="display-chip">
+                <Chip
+                  key={filter}
+                  label={filter}
+                />
+              </div>
+            ))}
+          </>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div>
@@ -26,17 +56,26 @@ function FilterTablePopover(props) {
           horizontal: 'center',
         }}
       >
-        <h1>HELLO WORLD</h1>
-        <h2>I AM GROOT</h2>
+        <Card>
+          <CardContent className="filter-table">
+            <div className="filter-table__title">
+              <Typography variant="h1"> Advanced Search Filter Groups</Typography>
+            </div>
+            <div className="filter-table__content">
+              {filterGroups.map(renderRow)}
+            </div>
+          </CardContent>
+        </Card>
       </Popover>
     </div>
   );
 }
 
 FilterTablePopover.propTypes = {
+  anchorEl: PropTypes.object.isRequired,
+  filterGroups: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
   handleToggle: PropTypes.func.isRequired,
   isOpen: PropTypes.bool.isRequired,
-  anchorEl: PropTypes.object.isRequired,
 };
 
 export default FilterTablePopover;

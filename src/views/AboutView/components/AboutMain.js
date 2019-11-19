74845826d0b516195d8365b0e1cfce2fd09ae879
Caralyn Reisle <creisle@bcgsc.ca>
2019-11-18 18:33:08 -0800

    Convert AboutMain to hooks

--- src/views/AboutView/components/AboutMain.js
+++ src/views/AboutView/components/AboutMain.js
@@ -1,104 +1,79 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Typography,
 } from '@material-ui/core';
 
 import api from '@/services/api';
-import { isAuthorized } from '@/services/auth';
-import { KBContext } from '@/components/KBContext';
 import {
   PieChart,
 } from '.';
 
 
-class AboutMain extends Component {
-  static contextType = KBContext;
+const AboutMain = () => {
+  const [chartData, setChartData] = useState({}); // label -> value
+  const [apiVersion, setApiVersion] = useState('');
+  const [dbVersion, setDbVersion] = useState('');
+  const guiVersion = process.env.npm_package_version || process.env.REACT_APP_VERSION || '';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      stats: [{ label: '', value: 0 }], // so that the page doesn't wait to load
-      apiVersion: '',
-      dbVersion: '',
-      guiVersion: process.env.npm_package_version || process.env.REACT_APP_VERSION || '',
+  // get the stats for the pie chart
+  useEffect(() => {
+    let controller;
+
+    const getData = async () => {
+      controller = api.get('/stats');
+      const result = await controller.request();
+      setChartData(result);
     };
-    this.controllers = [];
-  }
+    getData();
+    return () => controller && controller.abort();
+  }, []);
 
-  async componentDidMount() {
-    if (isAuthorized(this.context)) {
-      this.getClassStats();
-      this.getVersionInfo();
-    }
-  }
+  // get the version information
+  useEffect(() => {
+    let controller;
 
-  componentWillUnmount() {
-    this.controllers.forEach(c => c.abort());
-  }
+    const getData = async () => {
+      controller = api.get('/version');
+      const result = await controller.request();
+      setApiVersion(result.api);
+      setDbVersion(result.db);
+    };
+    getData();
+    return () => controller && controller.abort();
+  }, []);
 
-  async getClassStats() {
-    const call = api.get('/stats');
-    this.controllers.push(call);
 
-    const stats = await call.request();
+  const data = [];
+  Object.entries(chartData).forEach(([label, value]) => {
+    data.push({ label, value });
+  });
 
-    if (stats) {
-      this.setState({
-        stats: Array.from(
-          Object.keys(stats),
-          label => ({ label, value: stats[label] }),
-        ),
-      });
-    }
-  }
+  return (
+    <div className="about-page__content">
+      <div className="pie-partner">
+        <Typography paragraph>
+          Knowlegebase is a curated database of variants in cancer and their therapeutic,
+          biological, diagnostic, and prognostic implications according to literature. The
+          main use of Knowlegebase is to act as the link between the known and published
+          variant information and the expermientally collected data.
+        </Typography>
+        <Typography variant="h4">
+          Current Version
+        </Typography>
+        <Typography paragraph>
+          DB ({dbVersion}); API (v{apiVersion}); GUI (v{guiVersion})
+        </Typography>
 
-  async getVersionInfo() {
-    const call = api.get('/version');
-    this.controllers.push(call);
-    const versions = await call.request();
-    this.setState({
-      apiVersion: versions.api,
-      dbVersion: versions.db,
-    });
-  }
-
-  render() {
-    const {
-      stats, apiVersion, guiVersion, dbVersion,
-    } = this.state;
-
-    const countsByName = {};
-    stats.forEach(({ label, value }) => {
-      countsByName[label] = value;
-    });
-
-    return (
-      <div className="about-page__content">
-        <div className="pie-partner">
-          <Typography paragraph>
-            Knowlegebase is a curated database of variants in cancer and their therapeutic,
-            biological, diagnostic, and prognostic implications according to literature. The
-            main use of Knowlegebase is to act as the link between the known and published
-            variant information and the expermientally collected data.
-          </Typography>
-          <Typography variant="h4">
-            Current Version
-          </Typography>
-          <Typography paragraph>
-            DB ({dbVersion}); API (v{apiVersion}); GUI (v{guiVersion})
-          </Typography>
-
-        </div>
-        <PieChart
-          height={500}
-          width={500}
-          innerRadius={50}
-          data={stats}
-          colorThreshold={0.05}
-        />
       </div>
-    );
-  }
-}
+      <PieChart
+        height={500}
+        width={500}
+        innerRadius={50}
+        data={data}
+        colorThreshold={0.05}
+      />
+    </div>
+  );
+};
 
 export default AboutMain;

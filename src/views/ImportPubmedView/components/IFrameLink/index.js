13e4d9b3dfa45635c2bf383dd6bce01cf680b56c
Caralyn Reisle <creisle@bcgsc.ca>
2019-11-19 13:07:03 -0800

    Add view for importing pubmed records

---
+++ src/views/ImportPubmedView/components/IFrameLink/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { CircularProgress } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+
+import './index.scss';
+
+
+const IFrameLink = ({
+  url, title, className, ...opt
+}) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <div className={`iframe-link ${className}`}>
+      {isLoading && (<CircularProgress className="iframe-link__loader" />)}
+      <a href={url} target="_blank" rel="noopener noreferrer" className="iframe-link__link">
+        <iframe
+          width="100%"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+          {...opt}
+          scrolling="no"
+          title={title}
+          src={url}
+          onLoad={() => setIsLoading(false)}
+        />
+      </a>
+    </div>
+  );
+};
+
+IFrameLink.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+
+IFrameLink.defaultProps = {
+  className: '',
+};
+
+
+export default IFrameLink;

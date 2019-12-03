6f49ca15454a587cbe25c7d1173dc479d80c834b
Caralyn Reisle <creisle@bcgsc.ca>
2019-12-03 13:27:20 -0800

    Use common config function and separate dist paths

--- config/webpack/local.js
+++ config/webpack/local.js
@@ -3,10 +3,14 @@ const webpack = require('webpack');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 
-const common = require('./common.js');
+const path = require('path');
+
+const createBaseConfig = require('./common.js');
+
+const DIST = path.resolve(__dirname, '../../dist/production');
 
 
-const devConfig = {
+const config = {
   mode: 'development',
   plugins: [
     // Copy values of ENV variables in as strings using these defaults (null = unset)
@@ -36,4 +40,5 @@ const devConfig = {
     ],
   },
 };
-module.exports = merge(common, devConfig);
+
+module.exports = merge(createBaseConfig(DIST), config);

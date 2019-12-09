bcacc4994cde45ade294a36ef005872a8f272214
Caralyn Reisle <creisle@bcgsc.ca>
2019-12-09 14:47:05 -0800

    Convert FilteredRecordAutocomplete to hooks

--- src/components/FormField/FilteredRecordAutocomplete.js
+++ src/components/FormField/FilteredRecordAutocomplete.js
@@ -2,9 +2,8 @@ import './index.scss';
 
 import { FormControl, FormHelperText } from '@material-ui/core';
 import FilterIcon from '@material-ui/icons/FilterList';
-import { boundMethod } from 'autobind-decorator';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import RecordAutocomplete from '@/components/RecordAutocomplete';
 import ResourceSelectComponent from '@/components/ResourceSelectComponent';
@@ -15,119 +14,97 @@ import schema from '@/services/schema';
  * Allows an autocomplete record link to be filtered based on some class
  * model name to search by
  */
-class FilteredRecordAutocomplete extends React.PureComponent {
-  static propTypes = {
-    linkedClassName: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    DetailChipProps: PropTypes.object,
-    disabled: PropTypes.bool,
-    error: PropTypes.bool,
-    helperText: PropTypes.string,
-    isMulti: PropTypes.bool,
-  };
+const FilteredRecordAutocomplete = ({
+  linkedClassName,
+  disabled,
+  isMulti,
+  DetailChipProps,
+  helperText,
+  error,
+  name,
+  ...rest
+}) => {
+  const [selectedClassName, setSelectedClassName] = useState(linkedClassName);
 
-  static defaultProps = {
-    disabled: false,
-    isMulti: false,
-    helperText: '',
-    error: false,
-    DetailChipProps: {},
-  };
-
-  constructor(props) {
-    super(props);
-    const { linkedClassName } = this.props;
-    this.state = {
-      selectedClassName: linkedClassName,
-    };
-  }
-
-  componentDidUpdate(prevProps) {
-    const { linkedClassName } = this.props;
-
-    if (linkedClassName !== prevProps.linkedClassName) {
-      this.setState({ selectedClassName: linkedClassName });
-    }
-  }
-
-  @boundMethod
-  handleClassChange(event) {
+  const handleClassChange = useCallback((event) => {
     const { target: { value } } = event;
-    this.setState({ selectedClassName: value });
-  }
+    setSelectedClassName(value);
+  }, [setSelectedClassName]);
 
-  render() {
-    const {
-      disabled,
-      isMulti,
-      linkedClassName,
-      DetailChipProps,
-      helperText,
-      error,
-      name,
-      ...rest
-    } = this.props;
 
-    const {
-      selectedClassName,
-    } = this.state;
+  const model = schema.get(linkedClassName);
 
-    const model = schema.get(linkedClassName);
+  const itemToString = item => schema.getLabel(item);
 
-    const itemToString = item => schema.getLabel(item);
+  const searchHandler = api.defaultSuggestionHandler(
+    schema.get(selectedClassName),
+  );
 
-    const searchHandler = api.defaultSuggestionHandler(
-      schema.get(selectedClassName),
-    );
+  const valueToString = (record) => {
+    if (record && record['@rid']) {
+      return schema.getLabel(record, false);
+    }
+    if (Array.isArray(record)) {
+      return `Array(${record.length})`;
+    }
+    return `${record}`;
+  };
 
-    const valueToString = (record) => {
-      if (record && record['@rid']) {
-        return schema.getLabel(record, false);
-      }
-      if (Array.isArray(record)) {
-        return `Array(${record.length})`;
-      }
-      return `${record}`;
-    };
-
-    return (
-      <FormControl className="filtered-record-autocomplete" disabled={disabled} error={error}>
-        <div className="filtered-record-autocomplete__content">
-          {!disabled && (
+  return (
+    <FormControl className="filtered-record-autocomplete" disabled={disabled} error={error}>
+      <div className="filtered-record-autocomplete__content">
+        {!disabled && (
           <ResourceSelectComponent
             className="node-form__class-select filtered-record-autocomplete__select-search-class"
             IconComponent={FilterIcon}
             label={`Filter (${name}) Search by Class`}
             name="search-class"
-            onChange={this.handleClassChange}
+            onChange={handleClassChange}
             resources={[...model.descendantTree(false).map(m => m.name)]}
             value={selectedClassName}
           />
-          )}
-          <RecordAutocomplete
-            {...rest}
-            DetailChipProps={{
-              ...DetailChipProps,
-              valueToString,
-              getDetails: details => details,
-            }}
-            disabled={disabled}
-            getOptionKey={opt => opt['@rid']}
-            getOptionLabel={itemToString}
-            isMulti={isMulti}
-            name={name}
-            placeholder={isMulti
-              ? `Search for Existing ${selectedClassName} Record(s)`
-              : `Search for an Existing ${selectedClassName} Record`
+        )}
+        <RecordAutocomplete
+          {...rest}
+          DetailChipProps={{
+            ...DetailChipProps,
+            valueToString,
+            getDetails: details => details,
+          }}
+          disabled={disabled}
+          getOptionKey={opt => opt['@rid']}
+          getOptionLabel={itemToString}
+          isMulti={isMulti}
+          name={name}
+          placeholder={isMulti
+            ? `Search for Existing ${selectedClassName} Record(s)`
+            : `Search for an Existing ${selectedClassName} Record`
             }
-            searchHandler={searchHandler}
-          />
-        </div>
-        {helperText && (<FormHelperText>{helperText}</FormHelperText>)}
-      </FormControl>
-    );
-  }
-}
+          searchHandler={searchHandler}
+        />
+      </div>
+      {helperText && (<FormHelperText>{helperText}</FormHelperText>)}
+    </FormControl>
+  );
+};
+
+FilteredRecordAutocomplete.propTypes = {
+  linkedClassName: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  DetailChipProps: PropTypes.object,
+  disabled: PropTypes.bool,
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
+  isMulti: PropTypes.bool,
+};
+
+FilteredRecordAutocomplete.defaultProps = {
+  disabled: false,
+  isMulti: false,
+  helperText: '',
+  error: false,
+  DetailChipProps: {},
+};
 
 
 export default FilteredRecordAutocomplete;

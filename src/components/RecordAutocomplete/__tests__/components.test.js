3b7cd371032bfa164b43524328c09fae1b70c7a2
David Pham <dpa35@sfu.ca>
2019-12-19 16:00:20 -0800

    Strip away terms below min length from actual search

--- src/components/RecordAutocomplete/__tests__/components.test.js
+++ src/components/RecordAutocomplete/__tests__/components.test.js
@@ -1,9 +1,11 @@
 import '@testing-library/jest-dom/extend-expect';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitForElement } from '@testing-library/react';
 import React from 'react';
 
 import RecordAutocomplete from '..';
+
+const DOWN_ARROW = { keyCode: 40 };
 
 
 describe('RecordAutocomplete', () => {

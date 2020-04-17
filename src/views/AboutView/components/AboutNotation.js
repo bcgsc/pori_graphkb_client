b4f59f73827de26047b768f4f8d7825f7ebf5e99
Caralyn Reisle <creisle@bcgsc.ca>
2020-04-17 14:36:03 -0700

    refactor(hooks): convert to hooks

--- src/views/AboutView/components/AboutNotation.js
+++ src/views/AboutView/components/AboutNotation.js
@@ -1,61 +1,45 @@
 import notation from '@bcgsc/knowledgebase-parser/doc/notation.md';
 import marked from 'marked';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { LocationPropType } from '@/components/types';
 
 
-class AboutNotation extends React.Component {
-  static propTypes = {
-    location: LocationPropType.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: '',
-    };
-
-    this.controllers = [];
-  }
-
-  async componentDidMount() {
-    await this.getContent();
-  }
-
-  componentWillUnmount() {
-    this.controllers.forEach(c => c.abort());
-  }
+const AboutNotation = ({ location: { pathname } }) => {
+  const [content, setContent] = useState('');
 
   /**
    * Fetch the content from the file and convert it to html.
    * The links in the parsed markdown are all relative to the base url
    * and must be adjusted for the route location associated with this component
    */
-  async getContent() {
-    const { location: { pathname } } = this.props;
+  useEffect(() => {
+    let controller;
 
-    const controller = new AbortController();
-    this.controllers.push(controller);
+    const getContent = async () => {
+      controller = new AbortController();
+      const file = await fetch(notation, { signal: controller.signal });
+      const text = await file.text();
+      const htmlContent = marked(text);
+      setContent(htmlContent.replace(/href="(#[^"]+)"/g, `href="${pathname}$1"`));
+    };
 
-    const file = await fetch(notation, { signal: controller.signal });
-    const text = await file.text();
-    const htmlContent = marked(text);
-    this.setState({ content: htmlContent.replace(/href="(#[^"]+)"/g, `href="${pathname}$1"`) });
-  }
+    getContent();
+    return () => controller && controller.abort();
+  }, [pathname]);
 
-  render() {
-    const { content } = this.state;
+  return (
+    <div
+      className="about-page__content"
+      dangerouslySetInnerHTML={{ __html: content }}
+      id="about-notation"
+    />
+  );
+};
 
-    return (
-      <div
-        className="about-page__content"
-        dangerouslySetInnerHTML={{ __html: content }}
-        id="about-notation"
-      />
-    );
-  }
-}
+AboutNotation.propTypes = {
+  location: LocationPropType.isRequired,
+};
 
 
 export default AboutNotation;

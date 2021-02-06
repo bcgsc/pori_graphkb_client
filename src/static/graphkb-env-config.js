0b20d84fa4cdc09f5fa1fb0609d1b7f86e55eb12
Caralyn Reisle <creisle@bcgsc.ca>
2021-02-05 16:14:16 -0800

    Add default env-config script

---
+++ src/static/graphkb-env-config.js
@@ -0,0 +1,12 @@
+// default config.js file, gets replaced at run-time by env-scripts
+
+window._env_ = {
+  KEYCLOAK_CLIENT_ID: 'GraphKB',
+  KEYCLOAK_ROLE: 'GraphKB',
+  KEYCLOAK_REALM: 'GSC',
+  KEYCLOAK_URL: 'https://keycloakdev.bcgsc.ca/auth',
+  API_BASE_URL: 'https://graphkbdev-api.bcgsc.ca',
+  CONTACT_EMAIL: 'graphkb@bcgsc.ca',
+  CONTACT_TICKET_URL: 'https://www.bcgsc.ca/jira/projects/KBDEV',
+  PUBLIC_URL: '/',
+};

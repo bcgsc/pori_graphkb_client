daa6f5dfa9ab460cf8f2fe09cc2d06e14bb514de
Abbey Reisle <areisle@bcgsc.ca>
2022-02-25 08:13:05 -0800

    test: add types for useGrid

--- src/components/hooks/useGrid.tsx
+++ src/components/hooks/useGrid.tsx
@@ -1,14 +1,17 @@
-import { useMemo, useRef, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import {
+  LegacyRef, useMemo, useRef, useState,
+} from 'react';
 
 /**
  * hook for setting up and using ag-grids apis
  */
 const useGrid = () => {
-  const gridRef = useRef();
-  const [delayedRef, setDelayedRef] = useState(null);
+  const gridRef = useRef<AgGridReact>();
+  const [delayedRef, setDelayedRef] = useState<typeof gridRef | null>(null);
 
   const props = useMemo(() => ({
-    ref: gridRef,
+    ref: gridRef as LegacyRef<AgGridReact> | undefined,
     onGridReady: () => {
       setDelayedRef(gridRef);
     },

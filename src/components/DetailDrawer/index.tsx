bba98568bdc81902298bb9b0cbfc463f7828bab9
Eleanor Lewis <elewis@bcgsc.ca>
2024-05-02 16:42:02 -0700

    fix properties check in detaildrawer

--- src/components/DetailDrawer/index.tsx
+++ src/components/DetailDrawer/index.tsx
@@ -1,5 +1,5 @@
 import './index.scss';
-
+import { schema as schemaDefn } from '@bcgsc-pori/graphkb-schema';
 import {
   Collapse,
   Divider,
@@ -187,8 +187,8 @@ function DetailDrawer(props: DetailDrawerProps) {
     let properties = Object.keys(record)
       .map((key) => ({ name: key, type: util.parseKBType(record[key]) }));
 
-    if (schema && schema.getProperties(record)) {
-      properties = schema.getProperties(record);
+    if (record['@class'] && schemaDefn.getProperties(record['@class'])) {
+      properties = schema.getProperties(record['@class']);
     }
     const propsList = Object.values(properties)
       .filter((prop) => !identifiers.map((id) => id.split('.')[0]).includes(prop.name)

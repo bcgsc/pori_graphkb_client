936203690ac86102926acb43685c64eb6b53a4ec
Abbey Reisle <areisle@bcgsc.ca>
2026-04-09 18:42:15 -0700

    test: replace flaky test with stories
    KBDEV-1500

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,25 +3,41 @@ import ThemeProvider from '../src/theme';
 import { initialize, mswLoader, getWorker } from 'msw-storybook-addon'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import '../src/static/graphkb-env-config';
-import { HttpRequestHandler, HttpResponse, http as httpBase, isCommonAssetRequest } from 'msw';
+import { HttpResponse, ResponseResolverInfo, http as httpBase, isCommonAssetRequest } from 'msw';
 import { GeneralRecordType, QueryBody } from '../src/components/types';
-import { definePreview } from '@storybook/react-vite';
+import { Decorator, definePreview, StoryObj } from '@storybook/react-vite';
 import addonTest from '@storybook/addon-vitest';
 import { MswParameters } from 'msw-storybook-addon'
 import { PreviewAddon } from 'storybook/internal/csf';
+import { MemoryRouter } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
+import { AuthContext, AuthContextState } from '../src/components/Auth';
+import { expect, waitFor } from 'storybook/test';
+
+function wrapAsResponse<T extends Record<string, any> | any[]>(maybeResponse: T | HttpResponse<T>): HttpResponse<T> {
+  if (maybeResponse instanceof Response) {
+    return maybeResponse;
+  }
+  return HttpResponse.json(maybeResponse);
+}
+
+type AllowedResponse = Record<string, any> | any[] | HttpResponse<any>;
 
 export const http = {
   ...httpBase,
   gkb: {
-    get: (...args: Parameters<HttpRequestHandler>) => httpBase.get(`${window._env_.API_BASE_URL}${args[0]}`, args[1]),
-    query: (resolver: (body: QueryBody) => (HttpResponse<Partial<GeneralRecordType>[]> | Partial<GeneralRecordType>[])) => {
-      return httpBase.post(`${window._env_.API_BASE_URL}/api/query`, async ({ request }) => {
-        const body = await request.clone().json();
-        const response = resolver(body as any);
-        if (Array.isArray(response)) return HttpResponse.json(response);
-        return response;
+    get: (url: `/api/${string}`, resolver: (info: ResponseResolverInfo<any>) => AllowedResponse) => {
+      return httpBase.get(`${window._env_.API_BASE_URL}${url}`, (info) => {
+        return wrapAsResponse(resolver(info));
       })
-    }
+    },
+    post: (url: `/api/${string}`, resolver: (body: any, info: ResponseResolverInfo<any>) => AllowedResponse) => {
+      return httpBase.post(`${window._env_.API_BASE_URL}${url}`, async (info) => {
+        const body = await info.request.clone().json();
+        return wrapAsResponse(resolver(body, info));
+      })
+    },
+    query: (resolver: (body: QueryBody) => (HttpResponse<Partial<GeneralRecordType>[]> | Partial<GeneralRecordType>[])) => http.gkb.post('/api/query', resolver)
   }
 }
 
@@ -59,5 +75,60 @@ const preview = definePreview({
   ],
   loaders: [mswLoader, () => ({ queryClient: new QueryClient({ defaultOptions: { queries: { retry: false } } }) })],
 });
+
+export async function hasFinishedLoading({ step, canvas }: Pick<Parameters<NonNullable<StoryObj['play']>>[0], 'step' | 'canvas'>) {
+  await step('Finished Loading', async () => {
+    await waitFor(async () => {
+      await expect(canvas.queryAllByRole('progressbar')).toHaveLength(0);
+      await expect(canvas.queryAllByText(/loading\.\.\./i)).toHaveLength(0);
+    }, { timeout: 10000 });
+  });
+}
+
+export const withSnackbar: Decorator = (Story) => <SnackbarProvider anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}><Story /></SnackbarProvider>;
+
+interface WithAuthArgs {
+  auth?: Omit<Partial<AuthContextState>, 'user'> & { user?: Partial<AuthContextState['user']> };
+}
+
+export const withAuth: Decorator<WithAuthArgs> = (Story, { args }) => {
+    const auth = args.auth;
+    const authState: AuthContextState = {
+      login: () => { },
+      logout: () => { },
+      isAuthenticating: false,
+      isAuthenticated: true,
+      isAdmin: false,
+      hasWriteAccess: false,
+      error: undefined,
+      ...auth,
+      user: auth?.user === null ? undefined : {
+        signedLicenseAt: new Date(2020, 0, 1).getTime(),
+        name: 'bmonkeys',
+        groups: [],
+        '@rid': '#1:1',
+        ...auth?.user
+      },
+    }
+    return (
+          <AuthContext.Provider value={authState}>
+            <Story />
+          </AuthContext.Provider>
+    )
+};
+
+interface WithRouterArgs {
+  path?: string;
+}
+
+export const withRouter: Decorator<WithRouterArgs> = (Story, { args }) => {
+  const { path = '/' } = args;
+    return (
+      <MemoryRouter initialEntries={[path]}>
+        <Story />
+      </MemoryRouter>
+    )
+}
+
 
 export default preview;
---
+++ src/components/StatementForm/index.stories.tsx
@@ -0,0 +1,106 @@
+import {
+  expect, fn, screen, waitFor,
+} from 'storybook/test';
+
+import preview, {
+  hasFinishedLoading, http, withAuth, withRouter, withSnackbar,
+} from '#.storybook/preview';
+
+import { FORM_VARIANT } from '../util';
+import component from '.';
+
+const meta = preview.meta({
+  component,
+  decorators: [withSnackbar, withAuth, withRouter],
+  args: {
+    title: 'blargh monkeys',
+    onError: fn(),
+    onSubmit: fn(),
+    value: {},
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        http.gkb.query((payload) => {
+          // to prevent other records causing validation error when running `checkLogicalStatement`
+          if (payload.queryType === 'similarTo') {
+            return [];
+          }
+
+          return [
+            {
+              '@rid': '11:11',
+              displayName: 'anything',
+            },
+            {
+              '@rid': '12:23',
+              displayName: 'anything',
+            },
+            {
+              '@rid': '20:20',
+              displayName: 'anything',
+            },
+            {
+              '@rid': '90:32',
+              displayName: 'anything',
+            },
+          ];
+        }),
+        http.gkb.post('/api/statements', (body) => body),
+      ],
+    },
+  },
+});
+
+export const Editing = meta.story({
+  args: {
+    variant: FORM_VARIANT.EDIT,
+  },
+  play: async ({ canvas, step }) => {
+    await hasFinishedLoading({ canvas, step });
+    await expect(canvas.findByText('Add Review')).resolves.toBeInTheDocument();
+  },
+});
+
+export const New = meta.story({
+  args: {
+    variant: FORM_VARIANT.NEW,
+    auth: { hasWriteAccess: true, user: { '@rid': '23:9', name: 'name', groups: [] } },
+  },
+  play: hasFinishedLoading,
+});
+
+New.test('on submit sets reviewStatus as initial and adds empty review if left blank', async ({ canvas, userEvent, args }) => {
+  async function selectFromAutocomplete(label: RegExp, value: string) {
+    const input = await canvas.findByLabelText(label);
+    await userEvent.type(input, value[0]);
+    const option = await screen.findByText(value);
+    await userEvent.click(option);
+  }
+
+  await selectFromAutocomplete(/^conditions/, 'anything (11:11)');
+  await selectFromAutocomplete(/^evidence /, 'anything (12:23)');
+  await selectFromAutocomplete(/^relevance/, 'anything (90:32)');
+  await selectFromAutocomplete(/^subject/, 'anything (20:20)');
+  const submitBtn = canvas.getByText('SUBMIT');
+  expect(submitBtn).toBeEnabled();
+
+  await userEvent.click(submitBtn);
+
+  const expectedPayload = {
+    '@class': 'Statement',
+    conditions: ['11:11'],
+    evidence: ['12:23'],
+    relevance: '90:32',
+    subject: '20:20',
+    reviewStatus: 'initial',
+    reviews: [{
+      status: 'initial',
+      comment: '',
+      createdBy: '23:9',
+    }],
+  };
+  await waitFor(() => {
+    expect(args.onSubmit).toHaveBeenCalledWith(expectedPayload);
+  });
+});
